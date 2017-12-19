827adc1e33a07ed4d50bc54937c94bd8053eed2c
Josiah Savary <hello@jsavary.com>
2017-12-19 10:56:45 -0500

    Use latest release
    Removed hard-corded version number in release request

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 os=$([ $(uname | grep 'Darwin') ] && echo 'darwin' || echo 'linux');
-curl -s https://api.github.com/repos/livepeer/go-livepeer/releases/tags/0.1.7 \
+curl -s https://api.github.com/repos/livepeer/go-livepeer/releases/latest \
   | grep browser_download_url \
   | grep "${os}.tar" \
   | cut -d '"' -f 4 \
