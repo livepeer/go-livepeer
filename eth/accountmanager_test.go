0a8ad5ce43b94e3d4e06445726a7a94461c96ba7
Josh Allmann <joshua.allmann@gmail.com>
2018-01-19 20:51:26 -0800

    Add tests for password checking.

--- eth/accountmanager_test.go
+++ eth/accountmanager_test.go
@@ -17,7 +17,51 @@ func TestAccountManager(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	_, err = NewAccountManager(a.Address, dir)
+	am, err := NewAccountManager(a.Address, dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// ensure password checking works
+	err = am.Unlock("") // should prompt for pw. TODO expect-test this
+	if err != keystore.ErrDecrypt {
+		t.Fatal(err)
+	}
+
+	err = am.Unlock("foo!") // should not prompt for pw. TODO expect-test this
+	if err != keystore.ErrDecrypt {
+		t.Fatal(err)
+	}
+
+	err = am.Unlock("foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestEmptyPassphrase(t *testing.T) {
+	dir, ks := tmpKeyStore(t, true)
+	defer os.RemoveAll(dir)
+
+	a, err := ks.NewAccount("")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	am, err := NewAccountManager(a.Address, dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// This test ensures we don't prompt for pw. A bit artificial, but if
+	// we prompt, `go test` current semantics would mean getPassphrase
+	// returns an empty string, unlocking the wallet when it shouldn't.
+	err = am.Unlock("should not prompt for pw")
+	if err != keystore.ErrDecrypt {
+		t.Fatal(err)
+	}
+
+	err = am.Unlock("")
 	if err != nil {
 		t.Fatal(err)
 	}
