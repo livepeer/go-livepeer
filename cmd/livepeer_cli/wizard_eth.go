34642d2edf9fc0a04d67481a169a7b494f56cca7
Eric Tang <eric.x.tang@gmail.com>
2018-05-21 19:38:32 -0400

    Check round is not initialized before calling

---
+++ cmd/livepeer_cli/wizard_eth.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func (w *wizard) setGasPrice() {
+	fmt.Printf("Current gas price: %v\n", w.getGasPrice())
+	fmt.Printf("Enter new gas price (enter \"0\" for automatic)")
+	amount := w.readBigInt()
+
+	val := url.Values{
+		"amount": {fmt.Sprintf("%v", amount.String())},
+	}
+
+	httpPostWithParams(fmt.Sprintf("http://%v:%v/setGasPrice", w.host, w.httpPort), val)
+}
