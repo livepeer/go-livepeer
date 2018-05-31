bc18337f5fc56633d2fcbe487180272a504fb26f
Josh Allmann <joshua.allmann@gmail.com>
2018-05-30 23:37:48 -0700

    eth: Add flatten helper.

--- eth/types/segment.go
+++ eth/types/segment.go
@@ -16,3 +16,11 @@ type Segment struct {
 func (s *Segment) Hash() common.Hash {
 	return crypto.Keccak256Hash([]byte(s.StreamID), common.LeftPadBytes(s.SegmentSequenceNumber.Bytes(), 32), s.DataHash.Bytes())
 }
+
+func (s *Segment) Flatten() []byte {
+	buf := make([]byte, len(s.StreamID)+32+len(s.DataHash.Bytes()))
+	i := copy(buf[0:], []byte(s.StreamID))
+	i += copy(buf[i:], common.LeftPadBytes(s.SegmentSequenceNumber.Bytes(), 32))
+	i += copy(buf[i:], s.DataHash.Bytes())
+	return buf
+}
