0d91ee99adb07e2a32db7dedc97e9e03472179e9
Josh Allmann <joshua.allmann@gmail.com>
2018-05-30 23:37:48 -0700

    eth: Add flatten helper.

--- eth/types/segment_test.go
+++ eth/types/segment_test.go
@@ -4,7 +4,7 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
+	ethcommon "github.com/ethereum/go-ethereum/common"
 )
 
 func TestSegmentHash(t *testing.T) {
@@ -13,7 +13,7 @@ func TestSegmentHash(t *testing.T) {
 		segmentNumber = big.NewInt(0)
 		d0            = "QmR9BnJQisvevpCoSVWWKyownN58nydb2zQt9Z2VtnTnKe"
 
-		sHash = common.BytesToHash(common.FromHex("7fa493826bf6dc8fdd3e65ad6170193ec1a92cee5d78311953b3d0da928d7871"))
+		sHash = ethcommon.BytesToHash(ethcommon.FromHex("7fa493826bf6dc8fdd3e65ad6170193ec1a92cee5d78311953b3d0da928d7871"))
 	)
 
 	segment := &Segment{
@@ -24,5 +24,19 @@ func TestSegmentHash(t *testing.T) {
 
 	if segment.Hash() != sHash {
 		t.Fatalf("Invalid segment hash")
+	}
+}
+
+func TestSegmentFlatten(t *testing.T) {
+	// ensure that the flatten + manual hash results are identical to the
+	// segment hash results
+
+	s := Segment{
+		StreamID: "abcdef",
+		SegmentSequenceNumber: big.NewInt(1234),
+		DataHash: ethcommon.RightPadBytes([]byte("browns"), 32)
+	}
+	if !bytes.Equal(ethcommon.Keccak256(s.Flatten()), s.Hash().Bytes()) {
+		t.Error("Flattened segment + hash did not match segment hash function")
 	}
 }
