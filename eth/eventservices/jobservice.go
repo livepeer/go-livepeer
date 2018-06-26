330cf64282b5d8eabe1d3e762feb06aa06b4a4c7
Josh Allmann <joshua.allmann@gmail.com>
2018-06-26 15:13:14 -0700

    Clean up dead code.

--- eth/eventservices/jobservice.go
+++ eth/eventservices/jobservice.go
@@ -16,9 +16,6 @@ import (
 	"github.com/livepeer/go-livepeer/core"
 	"github.com/livepeer/go-livepeer/eth"
 	lpTypes "github.com/livepeer/go-livepeer/eth/types"
-	"github.com/livepeer/go-livepeer/net"
-	ffmpeg "github.com/livepeer/lpms/ffmpeg"
-	"github.com/livepeer/lpms/transcoder"
 )
 
 var (
@@ -119,116 +116,9 @@ func (s *JobService) IsWorking() bool {
 	return s.sub != nil
 }
 
-func (s *JobService) doTranscode(job *lpTypes.Job) (bool, error) {
-	//Check if broadcaster has enough funds
-	bDeposit, err := s.node.Eth.BroadcasterDeposit(job.BroadcasterAddress)
-	if err != nil {
-		glog.Errorf("Error getting broadcaster deposit: %v", err)
-		return false, err
-	}
-
-	if bDeposit.Cmp(job.MaxPricePerSegment) == -1 {
-		glog.Infof("Broadcaster does not have enough funds. Skipping job")
-		s.node.Database.SetStopReason(job.JobId, "Insufficient deposit")
-		return true, nil
-	}
-
-	//Create transcode config, make sure the profiles are sorted
-	config := net.TranscodeConfig{StrmID: job.StreamId, Profiles: job.Profiles, JobID: job.JobId, PerformOnchainClaim: true}
-	glog.Infof("Transcoder got job %v - strmID: %v, tData: %v, config: %v", job.JobId, job.StreamId, job.Profiles, config)
-
-	//Do The Transcoding
-	cm, err := s.node.GetClaimManager(job)
-	if err != nil {
-		glog.Error("Could not get claim manager: ", err)
-		return false, err
-	}
-	tr := transcoder.NewFFMpegSegmentTranscoder(job.Profiles, s.node.WorkDir)
-	strmIDs, err := s.node.TranscodeAndBroadcast(config, cm, tr)
-	if err != nil {
-		reason := fmt.Sprintf("Transcode error: %v", err)
-		glog.Errorf(reason)
-		s.node.Database.SetStopReason(job.JobId, reason)
-		return false, err
-	}
-
-	//Notify Broadcaster
-	sid := core.StreamID(job.StreamId)
-	vids := make(map[core.StreamID]ffmpeg.VideoProfile)
-	for i, vp := range job.Profiles {
-		vids[strmIDs[i]] = vp
-	}
-	if err = s.node.NotifyBroadcaster(sid.GetNodeID(), sid, vids); err != nil {
-		glog.Errorf("Notify Broadcaster Error: %v", err)
-		return true, nil
-	}
-
-	firstClaimBlock := new(big.Int).Add(job.CreationBlock, eth.BlocksUntilFirstClaimDeadline)
-	headersCh := make(chan *types.Header)
-	s.eventMonitor.SubscribeNewBlock(context.Background(), fmt.Sprintf("FirstClaimForJob%v", job.JobId), headersCh, func(h *types.Header) (bool, error) {
-		if cm.DidFirstClaim() {
-			// If the first claim has already been made then exit
-			return false, nil
-		}
-
-		// Check if current block is job creation block + 230
-		if h.Number.Cmp(firstClaimBlock) != -1 {
-			glog.Infof("Making the first claim")
-
-			canClaim, err := cm.CanClaim()
-			if err != nil {
-				return false, err
-			}
-
-			if canClaim {
-				err := cm.ClaimVerifyAndDistributeFees()
-				if err != nil {
-					return false, err
-				} else {
-					// If this claim was successful then the first claim has been made - exit
-					return false, nil
-				}
-			} else {
-				glog.Infof("No segments to claim")
-				// If there are no segments to claim at this point just stop watching
-				return false, nil
-			}
-		} else {
-			return true, nil
-		}
-	})
-
-	return true, nil
-}
-
 func (s *JobService) RestartTranscoder() error {
 
 	return eth.RecoverClaims(s.node.Eth, s.node.Ipfs, s.node.Database)
-	// Intentionally skip all the below. Remove after some grace period.
-
-	blknum, err := s.node.Eth.LatestBlockNum()
-	if err != nil {
-		return err
-	}
-	// fetch active jobs
-	jobs, err := s.node.Database.ActiveJobs(blknum)
-	if err != nil {
-		glog.Error("Could not fetch active jobs ", err)
-		return err
-	}
-	for _, j := range jobs {
-		job, err := s.node.Eth.GetJob(big.NewInt(j.ID)) // benchmark; may be faster to reconstruct locally?
-		if err != nil {
-			glog.Error("Unable to get job for ", j.ID, err)
-			continue
-		}
-		res, err := s.doTranscode(job)
-		if !res || err != nil {
-			glog.Error("Unable to restore transcoding of ", j.ID, err)
-			continue
-		}
-	}
-	return nil
 }
 
 func (s *JobService) firstClaim(job *lpTypes.Job) {
--- server/mediaserver.go
+++ server/mediaserver.go
@@ -71,7 +71,6 @@ type LivepeerServer struct {
 
 	rtmpStreams                map[core.StreamID]stream.RTMPVideoStream
 	hlsSubTimer                map[core.StreamID]time.Time
-	hlsWorkerRunning           bool
 	broadcastRtmpToHLSMap      map[string]string
 	broadcastRtmpToManifestMap map[string]string
 }
@@ -418,23 +417,6 @@ func gotRTMPStreamHandler(s *LivepeerServer) func(url *url.URL, rtmpStrm stream.
 			monitor.LogStreamCreatedEvent(mid.String(), nonce)
 		}
 
-		//Set up the transcode response callback
-		s.LivepeerNode.VideoNetwork.ReceivedTranscodeResponse(string(hlsStrmID), func(result map[string]string) {
-			//Parse through the results
-			for strmID, tProfile := range result {
-				vParams := ffmpeg.VideoProfileToVariantParams(ffmpeg.VideoProfileLookup[tProfile])
-				pl, _ := m3u8.NewMediaPlaylist(stream.DefaultHLSStreamWin, stream.DefaultHLSStreamCap)
-				variant := &m3u8.Variant{URI: fmt.Sprintf("%v.m3u8", strmID), Chunklist: pl, VariantParams: vParams}
-				manifest.Append(variant.URI, variant.Chunklist, variant.VariantParams)
-			}
-
-			//Update the master playlist on the network
-			if err = s.LivepeerNode.VideoNetwork.UpdateMasterPlaylist(string(mid), manifest); err != nil {
-				glog.Errorf("Error updating master playlist on network: %v", err)
-				return
-			}
-		})
-
 		glog.Infof("\n\nVideo Created With ManifestID: %v\n\n", mid)
 		glog.V(common.SHORT).Infof("\n\nhlsStrmID: %v\n\n", hlsStrmID)
 
@@ -594,8 +576,6 @@ func getRTMPStreamHandler(s *LivepeerServer) func(url *url.URL) (stream.RTMPVide
 
 // //HLS video streams need to be timed out.  When the viewer is no longer watching the stream, we send a Unsubscribe message to the network.
 func (s *LivepeerServer) startHlsUnsubscribeWorker(limit time.Duration, freq time.Duration) {
-	s.hlsWorkerRunning = true
-	defer func() { s.hlsWorkerRunning = false }()
 	for {
 		time.Sleep(freq)
 		for sid, t := range s.hlsSubTimer {
