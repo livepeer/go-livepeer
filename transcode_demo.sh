3ea50bfd2cde812f2c060b1e64b1595748e1c3f5
Eric Tang <eric.x.tang@gmail.com>
2018-11-10 13:04:13 -0500

    new demo script

--- transcode_demo.sh
+++ transcode_demo.sh
@@ -1,10 +1,4 @@
-strmID=$(curl localhost:8935/streamID 2> /dev/null)
-echo $strmID
-curl "http://localhost:8935/transcode?strmID=$strmID"
-sleep 2
-
-manifestID=$(curl localhost:8935/manifestID 2> /dev/null)
-manifest=$(curl localhost:8935/stream/$manifestID.m3u8 2> /dev/null)
+manifest=$(curl -s 'localhost:7935/status' | jq -r '.[][]' 2> /dev/null)
 sid1=$(echo $manifest| cut -d' ' -f 4)
 sid2=$(echo $manifest| cut -d' ' -f 6)
 sid3=$(echo $manifest| cut -d' ' -f 8)
