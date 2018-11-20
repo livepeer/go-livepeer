0989c9e414256d08354d91f19dd60013810f6e5b
Josh Allmann <joshua.allmann@gmail.com>
2018-11-20 09:10:42 -0800

    tests: O/T split tests.

--- core/orch_test.go
+++ core/orch_test.go
@@ -1,16 +1,22 @@
 package core
 
 import (
+	"context"
+	"fmt"
 	"io/ioutil"
 	"math/big"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/livepeer/go-livepeer/common"
 	"github.com/livepeer/go-livepeer/eth"
 	lpTypes "github.com/livepeer/go-livepeer/eth/types"
+	"github.com/livepeer/go-livepeer/net"
 
 	ethcommon "github.com/ethereum/go-ethereum/common"
+
+	"google.golang.org/grpc/metadata"
 )
 
 func TestCurrentBlock(t *testing.T) {
@@ -129,4 +135,176 @@ func TestGetJob(t *testing.T) {
 	if err != nil || j.TranscoderAddress != sj.TranscoderAddress {
 		t.Error("Got error or unexpected transcoder address ", err, j.TranscoderAddress)
 	}
+}
+
+func TestServeTranscoder(t *testing.T) {
+	n, _ := NewLivepeerNode(nil, "", nil)
+	strm := &StubTranscoderServer{}
+
+	// test that a transcoder was created
+	go n.serveTranscoder(strm)
+	time.Sleep(1 * time.Second)
+	if n.Transcoder == nil {
+		t.Error("Transcoder nil")
+	}
+	tc, ok := n.Transcoder.(*RemoteTranscoder)
+	if !ok {
+		t.Error("Unexpected transcoder type")
+	}
+
+	// test shutdown
+	tc.eof <- struct{}{}
+	time.Sleep(1 * time.Second)
+	if n.Transcoder != nil {
+		t.Error("Transcoder not nil")
+	}
+}
+
+func TestRemoteTranscoder(t *testing.T) {
+	n, _ := NewLivepeerNode(nil, "", nil)
+	initTranscoder := func() (*RemoteTranscoder, *StubTranscoderServer) {
+		strm := &StubTranscoderServer{node: n}
+		tc := NewRemoteTranscoder(n, strm)
+		return tc, strm
+	}
+
+	// happy path
+	tc, strm := initTranscoder()
+	res, err := tc.Transcode("", nil)
+	if err != nil || string(res[0]) != "asdf" {
+		t.Error("Error transcoding ", err)
+	}
+
+	// error on remote while transcoding
+	tc, strm = initTranscoder()
+	strm.TranscodeError = fmt.Errorf("TranscodeError")
+	res, err = tc.Transcode("", nil)
+	if err != strm.TranscodeError {
+		t.Error("Unexpected error ", err, res)
+	}
+
+	// simulate error with sending
+	tc, strm = initTranscoder()
+	strm.SendError = fmt.Errorf("SendError")
+	_, err = tc.Transcode("", nil)
+	if err != strm.SendError {
+		t.Error("Unexpected error ", err)
+	}
+
+	// simulate timeout
+	tc, strm = initTranscoder()
+	strm.WithholdResults = true
+	RemoteTranscoderTimeout = 1 * time.Millisecond
+	_, err = tc.Transcode("", nil)
+	if err.Error() != "Remote transcoder took too long" {
+		t.Error("Unexpected error ", err)
+	}
+	RemoteTranscoderTimeout = 8 * time.Second
+}
+
+func TestTaskChan(t *testing.T) {
+	n, _ := NewLivepeerNode(nil, "", nil)
+	// Sanity check task ID
+	if n.taskCount != 0 {
+		t.Error("Unexpected taskid")
+	}
+	if len(n.taskChans) != int(n.taskCount) {
+		t.Error("Unexpected task chan length")
+	}
+
+	// Adding task chans
+	const MaxTasks = 1000
+	for i := 0; i < MaxTasks; i++ {
+		go n.addTaskChan() // hopefully concurrently...
+	}
+	for j := 0; j < 10; j++ {
+		n.taskMutex.RLock()
+		tid := n.taskCount
+		n.taskMutex.RUnlock()
+		if tid >= MaxTasks {
+			break
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+	if n.taskCount != MaxTasks {
+		t.Error("Time elapsed")
+	}
+	if len(n.taskChans) != int(n.taskCount) {
+		t.Error("Unexpected task chan length")
+	}
+
+	// Accessing task chans
+	existingIds := []int64{0, 1, MaxTasks / 2, MaxTasks - 2, MaxTasks - 1}
+	for _, id := range existingIds {
+		_, err := n.getTaskChan(int64(id))
+		if err != nil {
+			t.Error("Unexpected error getting task chan for ", id, err)
+		}
+	}
+	missingIds := []int64{-1, MaxTasks}
+	testNonexistentChans := func(ids []int64) {
+		for _, id := range ids {
+			_, err := n.getTaskChan(int64(id))
+			if err == nil || err.Error() != "No transcoder channel" {
+				t.Error("Did not get expected error for ", id, err)
+			}
+		}
+	}
+	testNonexistentChans(missingIds)
+
+	// Removing task chans
+	for i := 0; i < MaxTasks; i++ {
+		go n.removeTaskChan(int64(i)) // hopefully concurrently...
+	}
+	for j := 0; j < 10; j++ {
+		n.taskMutex.RLock()
+		tlen := len(n.taskChans)
+		n.taskMutex.RUnlock()
+		if tlen <= 0 {
+			break
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+	if len(n.taskChans) != 0 {
+		t.Error("Time elapsed")
+	}
+	testNonexistentChans(existingIds) // sanity check for removal
+}
+
+type StubTranscoderServer struct {
+	node            *LivepeerNode
+	SendError       error
+	TranscodeError  error
+	WithholdResults bool
+
+	StubServerStream
+}
+
+func (s *StubTranscoderServer) Send(n *net.NotifySegment) error {
+	res := RemoteTranscoderResult{Segments: [][]byte{[]byte("asdf")}, Err: s.TranscodeError}
+	if !s.WithholdResults {
+		s.node.transcoderResults(n.TaskId, &res)
+	}
+	return s.SendError
+}
+
+type StubServerStream struct {
+}
+
+func (s *StubServerStream) Context() context.Context {
+	return context.Background()
+}
+func (s *StubServerStream) SetHeader(md metadata.MD) error {
+	return nil
+}
+func (s *StubServerStream) SendHeader(md metadata.MD) error {
+	return nil
+}
+func (s *StubServerStream) SetTrailer(md metadata.MD) {
+}
+func (s *StubServerStream) SendMsg(m interface{}) error {
+	return nil
+}
+func (s *StubServerStream) RecvMsg(m interface{}) error {
+	return nil
 }
---
+++ core/transcoder_test.go
@@ -0,0 +1,31 @@
+package core
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/livepeer/lpms/ffmpeg"
+)
+
+func TestLocalTranscoder(t *testing.T) {
+	tmp, _ := ioutil.TempDir("", "")
+	defer os.RemoveAll(tmp)
+	tc := NewLocalTranscoder(tmp)
+	ffmpeg.InitFFmpeg()
+
+	profiles := []ffmpeg.VideoProfile{ffmpeg.P144p30fps16x9, ffmpeg.P240p30fps16x9}
+	res, err := tc.Transcode("test.ts", profiles)
+	if err != nil {
+		t.Error("Error transcoding ", err)
+	}
+	if len(res) != len(profiles) {
+		t.Error("Mismatched results")
+	}
+	if Over1Pct(len(res[0]), 155476) {
+		t.Errorf("Wrong data %v", len(res[0]))
+	}
+	if Over1Pct(len(res[1]), 208304) {
+		t.Errorf("Wrong data %v", len(res[1]))
+	}
+}
