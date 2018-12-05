bbc17055aa577224a948c48707c8aeb9938028f3
Josh Allmann <joshua.allmann@gmail.com>
2018-12-05 12:57:44 -0800

    core: Re-enable tests.

--- core/livepeernode_test.go
+++ core/livepeernode_test.go
@@ -3,42 +3,25 @@ package core
 import (
 	"fmt"
 	"io/ioutil"
-	"math/big"
-	"time"
+	"os"
+	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	lpTypes "github.com/livepeer/go-livepeer/eth/types"
-	ffmpeg "github.com/livepeer/lpms/ffmpeg"
+	"github.com/livepeer/go-livepeer/drivers"
+	"github.com/livepeer/lpms/ffmpeg"
 )
-
-type StubClaimManager struct {
-	verifyCalled         bool
-	distributeFeesCalled bool
-	receiptAdded         bool
-}
-
-func (cm *StubClaimManager) BroadcasterAddr() common.Address { return common.Address{} }
-func (cm *StubClaimManager) AddReceipt(seqNo int64, fname string, data []byte, bSig []byte, tData map[ffmpeg.VideoProfile][]byte, tStart time.Time, tEnd time.Time) ([]byte, error) {
-	cm.receiptAdded = true
-	return []byte{}, nil
-}
-func (cm *StubClaimManager) SufficientBroadcasterDeposit() (bool, error)   { return true, nil }
-func (cm *StubClaimManager) ClaimVerifyAndDistributeFees() error           { return nil }
-func (cm *StubClaimManager) CanClaim(*big.Int, *lpTypes.Job) (bool, error) { return true, nil }
 
 type StubTranscoder struct {
 	Profiles      []ffmpeg.VideoProfile
-	InputData     [][]byte
+	SegCount      int
 	FailTranscode bool
 }
 
 func (t *StubTranscoder) Transcode(fname string, profiles []ffmpeg.VideoProfile) ([][]byte, error) {
-	d, err := ioutil.ReadFile(fname)
-	if err != nil || t.FailTranscode {
+	if t.FailTranscode {
 		return nil, ErrTranscode
 	}
 
-	t.InputData = append(t.InputData, d)
+	t.SegCount++
 
 	result := make([][]byte, 0)
 	for _, p := range t.Profiles {
@@ -47,24 +30,15 @@ func (t *StubTranscoder) Transcode(fname string, profiles []ffmpeg.VideoProfile)
 	return result, nil
 }
 
-/*
- XXX fix this
 func TestTranscodeAndBroadcast(t *testing.T) {
-	strmID, _ := MakeStreamID(RandomVideoID(), ffmpeg.P720p30fps4x3.Name)
-	jid := big.NewInt(0)
 	ffmpeg.InitFFmpeg()
 	p := []ffmpeg.VideoProfile{ffmpeg.P720p60fps16x9, ffmpeg.P144p30fps16x9}
 	tr := &StubTranscoder{Profiles: p}
-	mkid := func(p ffmpeg.VideoProfile) StreamID {
-		s, _ := MakeStreamID(strmID.GetVideoID(), p.Name)
-		return s
-	}
-	strmIds := []StreamID{mkid(p[0]), mkid(p[1])}
-	cm := StubClaimManager{}
-	config := transcodeConfig{StrmID: strmID.String(), Profiles: p, ResultStrmIDs: strmIds, ClaimManager: &cm, JobID: jid}
+	storage := drivers.NewMemoryDriver("").NewSession("")
+	config := transcodeConfig{LocalOS: storage, OS: storage}
 
 	tmpdir, _ := ioutil.TempDir("", "")
-	n, err := NewLivepeerNode(&eth.StubClient{}, tmpdir, nil)
+	n, err := NewLivepeerNode(nil, tmpdir, nil)
 	if err != nil {
 		t.Errorf("Error: %v", err)
 	}
@@ -82,13 +56,14 @@ func TestTranscodeAndBroadcast(t *testing.T) {
 		t.Errorf("Expecting %v profiles, got %v", len(p), len(res.Data))
 	}
 
-	//Should have transcoded the segments into 2 different profiles (right now StubSubscriber emits 1 segment)
-	if len(tr.InputData) != 1 {
-		t.Errorf("Expecting 1 segment to be transcoded, got %v", tr.InputData)
+	//Should have 1 transcoded segment into 2 different profiles
+	if tr.SegCount != 1 {
+		t.Errorf("Expecting 1 segment to be transcoded, got %v", tr.SegCount)
 	}
 
 	// Check playlist was updated
-	for _, v := range strmIds {
+	// XXX Fix this
+	/*for _, v := range strmIds {
 		pl := n.VideoSource.GetHLSMediaPlaylist(v)
 		if pl == nil {
 			t.Error("Expected media playlist; got none")
@@ -96,11 +71,8 @@ func TestTranscodeAndBroadcast(t *testing.T) {
 		if len(pl.Segments) != 1 && pl.SeqNo != 100 {
 			t.Error("Mismatched segments (expected 1) or seq (expected 100), got ", pl.Segments, pl.SeqNo)
 		}
-	}
+	}*/
 
-	if !cm.receiptAdded {
-		t.Error("Receipt was not added ", cm.receiptAdded)
-	}
 	// TODO check sig?
 
 	// Test when transcoder fails
@@ -118,8 +90,4 @@ func TestTranscodeAndBroadcast(t *testing.T) {
 		t.Error("Did not get mismatched segments as expected")
 	}
 	tr.Profiles = p
-
-	//TODO: Should have done the claiming
-
 }
-*/
