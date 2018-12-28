661f2c9998daf8f3681e2bdee0cc27c28145e41d
Yondon Fu <yondon.fu@gmail.com>
2018-12-28 11:21:41 -0500

    eth + pm: client implements Broker
    - Use a single getter for deposit/penalty escrow in Broker
    - Add transaction to return signature of Broker functions
    - Add wrapper functions to client to implement Broker

--- pm/broker.go
+++ pm/broker.go
@@ -4,6 +4,7 @@ import (
 	"math/big"
 
 	ethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 )
 
 // Broker is an interface which serves as an abstraction over an on-chain
@@ -12,34 +13,34 @@ import (
 type Broker interface {
 	// FundAndApproveSigners funds a sender's deposit and penalty escrow in addition
 	// to approving a set of ETH addresses to sign on behalf of the sender
-	FundAndApproveSigners(depositAmount *big.Int, penaltyEscrowAmount *big.Int, signers []ethcommon.Address) error
+	FundAndApproveSigners(depositAmount *big.Int, penaltyEscrowAmount *big.Int, signers []ethcommon.Address) (*types.Transaction, error)
 
 	// FundDeposit funds a sender's deposit
-	FundDeposit(amount *big.Int) error
+	FundDeposit(amount *big.Int) (*types.Transaction, error)
 
 	// FundPenaltyEscrow funds a sender's penalty escrow
-	FundPenaltyEscrow(amount *big.Int) error
+	FundPenaltyEscrow(amount *big.Int) (*types.Transaction, error)
 
 	// ApproveSigners approves a set of ETH addresses to sign on behalf of the sender
-	ApproveSigners(signers []ethcommon.Address) error
+	ApproveSigners(signers []ethcommon.Address) (*types.Transaction, error)
 
 	// RequestSignersRevocation requests the revocation of a set of approved ETH address signers
-	RequestSignersRevocation(signers []ethcommon.Address) error
+	RequestSignersRevocation(signers []ethcommon.Address) (*types.Transaction, error)
 
 	// Unlock initiates the unlock period for a sender after which a sender can withdraw its
 	// deposit and penalty escrow
-	Unlock() error
+	Unlock() (*types.Transaction, error)
 
 	// CancelUnlock stops a sender's active unlock period
-	CancelUnlock() error
+	CancelUnlock() (*types.Transaction, error)
 
 	// Withdraw credits a sender with its deposit and penalty escrow after the sender
 	// waits through the unlock period
-	Withdraw() error
+	Withdraw() (*types.Transaction, error)
 
 	// RedeemWinningTicket submits a ticket to be validated by the broker and if a valid winning ticket
 	// the broker pays the ticket's face value to the ticket's recipient
-	RedeemWinningTicket(ticket *Ticket, sig []byte, recipientRand *big.Int) error
+	RedeemWinningTicket(ticket *Ticket, sig []byte, recipientRand *big.Int) (*types.Transaction, error)
 
 	// IsUsedTicket checks if a ticket has been used
 	IsUsedTicket(ticket *Ticket) (bool, error)
@@ -47,9 +48,10 @@ type Broker interface {
 	// IsApprovedSigner checks if a ETH address signer is approved for a sender
 	IsApprovedSigner(sender ethcommon.Address, signer ethcommon.Address) (bool, error)
 
-	// GetDeposit returns the deposit value managed by the broker for a specified user
-	GetDeposit(addr ethcommon.Address) (*big.Int, error)
-
-	// GetPenaltyEscrow returns the penalty escrow value managed by the broker for a specified user
-	GetPenaltyEscrow(addr ethcommon.Address) (*big.Int, error)
+	// Senders returns a sender's information
+	Senders(addr ethcommon.Address) (struct {
+		Deposit       *big.Int
+		PenaltyEscrow *big.Int
+		WithdrawBlock *big.Int
+	}, error)
 }
