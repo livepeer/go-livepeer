54e0b352c0446d40baca9a20d785bfde1dd99527
Eli Mallon <eli@stream.place>
2019-05-16 14:51:49 -0700

    Update install_ffmpeg.sh

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,9 +1,24 @@
+
 #!/usr/bin/env bash
 
 set -ex
 
 export PATH="$HOME/compiled/bin":$PATH
 export PKG_CONFIG_PATH=$HOME/compiled/lib/pkgconfig
+
+# Windows (MSYS2) needs a few tweaks
+if [[ $(uname) == *"MSYS_NT"* ]]; then
+  export PATH="$PATH:/usr/bin:/mingw64/bin"
+  export C_INCLUDE_PATH="${C_INCLUDE_PATH:-}:/msys64/mingw64/lib"
+  export HOME="/build"
+  mkdir -p $HOME
+
+  export PATH="$HOME/compiled/bin":$PATH
+  export PKG_CONFIG_PATH=$HOME/compiled/lib/pkgconfig:/mingw64/lib/pkgconfig
+
+  export TARGET_OS="--target-os=mingw64"
+  export HOST_OS="--host=x86_64-w64-mingw32"
+fi
 
 # NVENC only works on Windows/Linux
 if [ $(uname) != "Darwin" ]; then
