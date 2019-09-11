22f163ef89a7b172c7b07867fd27d0e256af6ef2
Ivan Tivonenko <darkdarkdragon@gmail.com>
2019-09-11 10:32:44 +0300

    fix tests

--- test.sh
+++ test.sh
@@ -3,47 +3,47 @@
 #Test script to run all the tests for continuous integration
 
 cd core
-go test -logtostderr=true
+go test -logtostderr=true -v
 t1=$?
 cd ..
 
 cd server
-go test -logtostderr=true
+go test -logtostderr=true -v
 t2=$?
 cd ..
 
 cd monitor
-go test -logtostderr=true
+go test -logtostderr=true -v
 t3=$?
 cd ..
 
 cd eth
-go test -logtostderr=true
+go test -logtostderr=true -v
 t4=$?
 cd ..
 
 cd eth/watchers
-go test -logtostderr=true
+go test -logtostderr=true -v
 t5=$?
 cd ../..
 
 cd common
-go test -logtostderr=true
+go test -logtostderr=true -v
 t6=$?
 cd ..
 
 cd discovery
-go test -logtostderr=true
+go test -logtostderr=true -v
 t7=$?
 cd ..
 
 cd pm
-go test -logtostderr=true
+go test -logtostderr=true -v
 t8=$?
 cd ..
 
 cd drivers
-go test -logtostderr=true
+go test -logtostderr=true -v
 t9=$?
 cd ..
 
