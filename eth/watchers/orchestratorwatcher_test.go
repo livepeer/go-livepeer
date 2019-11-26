0f6d90747f01eb975cc1e1a1536d47cf2889f1d9
Nico Vergauwen <nico@Nicos-MacBook-Pro.local>
2019-11-27 00:24:24 +0100

    eth/watchers: store serviceURI on TranscoderActivated event

--- eth/watchers/orchestratorwatcher_test.go
+++ eth/watchers/orchestratorwatcher_test.go
@@ -1,10 +1,12 @@
 package watchers
 
 import (
+	"errors"
 	"math/big"
 	"testing"
 	"time"
 
+	"github.com/golang/glog"
 	"github.com/livepeer/go-livepeer/eth"
 	"github.com/livepeer/go-livepeer/eth/blockwatch"
 	lpTypes "github.com/livepeer/go-livepeer/eth/types"
@@ -38,6 +40,7 @@ func TestOrchWatcher_HandleLog_TranscoderActivated(t *testing.T) {
 			Address:           pm.RandAddress(),
 			ActivationRound:   big.NewInt(5),
 			DeactivationRound: big.NewInt(100),
+			ServiceURI:        "http://mytranscoder.lpt:1337",
 		},
 	}
 	ow, err := NewOrchestratorWatcher(stubBondingManagerAddr, watcher, stubStore, lpEth)
@@ -60,13 +63,34 @@ func TestOrchWatcher_HandleLog_TranscoderActivated(t *testing.T) {
 	assert.Equal(stubActivationRound.Int64(), stubStore.activationRound)
 	assert.Equal(stubStore.deactivationRound, maxFutureRound)
 	assert.Equal(stubStore.ethereumAddr, stubTranscoder.String())
+	assert.Equal(stubStore.serviceURI, "http://mytranscoder.lpt:1337")
+
+	// test GetServiceURI error
+	errorLogsBefore := glog.Stats.Error.Lines()
+	lpEth.Err = errors.New("GetServiceURI error")
+	watcher.sink <- []*blockwatch.Event{blockEvent}
+	time.Sleep(2 * time.Millisecond)
+	errorLogsAfter := glog.Stats.Error.Lines()
+	assert.Equal(int64(1), errorLogsAfter-errorLogsBefore)
+	lpEth.Err = nil
 
 	blockEvent.Type = blockwatch.Removed
+	lpEth.Orch.ServiceURI = "http://mytranscoder.lpt:0000"
 	watcher.sink <- []*blockwatch.Event{blockEvent}
 	time.Sleep(2 * time.Millisecond)
 	assert.Equal(stubStore.activationRound, int64(5))
 	assert.Equal(stubStore.deactivationRound, int64(100))
 	assert.Equal(stubStore.ethereumAddr, lpEth.Orch.Address.String())
+	assert.Equal(stubStore.serviceURI, "http://mytranscoder.lpt:0000")
+
+	// test GetTranscoder error
+	errorLogsBefore = glog.Stats.Error.Lines()
+	lpEth.Err = errors.New("GetTranscoder error")
+	watcher.sink <- []*blockwatch.Event{blockEvent}
+	time.Sleep(2 * time.Millisecond)
+	errorLogsAfter = glog.Stats.Error.Lines()
+	assert.Equal(int64(1), errorLogsAfter-errorLogsBefore)
+	lpEth.Err = nil
 }
 
 func TestOrchWatcher_HandleLog_TranscoderDeactivated(t *testing.T) {
