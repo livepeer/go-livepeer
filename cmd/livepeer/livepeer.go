be516a65666ca14170a66f100d21dd0a49628d81
Eli Mallon <eli@livepeer.org>
2020-03-12 18:03:44 -0700

    cpu retry hack: do it with cpu transcodes too

--- cmd/livepeer/livepeer.go
+++ cmd/livepeer/livepeer.go
@@ -253,6 +253,8 @@ func main() {
 			n.BackupTranscoder = n.TranscoderManager
 		} else {
 			n.Transcoder = core.NewLocalTranscoder(*datadir)
+			n.TranscoderManager = core.NewRemoteTranscoderManager()
+			n.BackupTranscoder = n.TranscoderManager
 		}
 	}
 
