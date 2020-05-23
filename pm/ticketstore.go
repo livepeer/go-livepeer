433eb12507fd3e4c9cc4a4c6195eb994afc5698c
Nico Vergauwen <nico@Nicos-MacBook-Pro.local>
2020-05-23 17:13:09 +0200

    fixup! pm: change ticketstore API to reflect DB changes

--- pm/ticketstore.go
+++ pm/ticketstore.go
@@ -5,7 +5,7 @@ import ethcommon "github.com/ethereum/go-ethereum/common"
 // TicketStore is an interface which describes an object capable
 // of persisting tickets
 type TicketStore interface {
-	LoadLatestTicket(sender ethcommon.Address) (*SignedTicket, error)
+	SelectEarliestWinningTicket(sender ethcommon.Address) (*SignedTicket, error)
 
 	// RemoveWinningTicket removes a ticket from the TicketStore
 	RemoveWinningTicket(ticket *SignedTicket) error
