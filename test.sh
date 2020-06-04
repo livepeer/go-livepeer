fea9f1091159705cb3f95958e8d65b2ef1bfa0e1
Josh Allmann <joshua.allmann@gmail.com>
2020-06-04 16:01:22 -0700

    test: Simplify test script.

--- test.sh
+++ test.sh
@@ -1,37 +1,28 @@
 #!/usr/bin/env bash
 
+set -eux
+
 #Test script to run all the tests for continuous integration
 
-go test ./... -v
-t1=$?
+go test ./...
 
 cd core
 # Be more strict with load balancer tests: run with race detector enabled
-go test -logtostderr=true -run LB_ -race
-t2_lb=$?
+go test -run LB_ -race
 # Be more strict with nvidia tests: run with race detector enabled
-go test -logtostderr=true -run Nvidia_ -race
-t2_nv=$?
+go test -run Nvidia_ -race
 cd ..
+
 # Be more strict with discovery tests: run with race detector enabled
 cd discovery
-go test -logtostderr=true -race
-t_discovery=$?
+go test -race
 cd ..
+
 # Be more strict with HTTP push tests: run with race detector enabled
 cd server
-go test -logtostderr=true -run Push_ -race
-t_push=$?
+go test -run Push_ -race
 cd ..
 
 ./test_args.sh
-t_args=$?
 
-if (($t1!=0||$t2_lb!=0||$t2_nv!=0||$t_args!=0||$t_discovery!=0||$t_push!=0))
-then
-    printf "\n\nSome Tests Failed\n\n"
-    exit -1
-else
-    printf "\n\nAll Tests Passed\n\n"
-    exit 0
-fi
+printf "\n\nAll Tests Passed\n\n"
