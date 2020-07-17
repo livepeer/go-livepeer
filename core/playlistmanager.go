7ac38d2b9945b85781dd6a9e8225be2525c26d33
Ivan Tivonenko <darkdarkdragon@gmail.com>
2020-07-17 22:59:48 +0300

    Save json playlist

--- core/playlistmanager.go
+++ core/playlistmanager.go
@@ -1,8 +1,10 @@
 package core
 
 import (
+	"encoding/json"
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/golang/glog"
 	"github.com/livepeer/go-livepeer/drivers"
@@ -27,6 +29,8 @@ type PlaylistManager interface {
 
 	GetRecordOSSession() drivers.OSSession
 
+	FlushRecord()
+
 	Cleanup()
 }
 
@@ -37,7 +41,41 @@ type BasicPlaylistManager struct {
 	// Live playlist used for broadcasting
 	masterPList *m3u8.MasterPlaylist
 	mediaLists  map[string]*m3u8.MediaPlaylist
+	jsonList    *jsonPlaylist
 	mapSync     *sync.RWMutex
+}
+
+type jsonSeg struct {
+	SeqNo      uint64 `json:"seq_no,omitempty"`
+	URI        string `json:"uri,omitempty"`
+	DurationMS uint64 `json:"duration_ms,omitempty"`
+}
+
+type jsonPlaylist struct {
+	name       string
+	DurationMs uint64               `json:"duration_ms,omitempty"` // total duration of the saved sagments
+	Segments   map[string][]jsonSeg `json:"segments,omitempty"`
+}
+
+func newJSONPlaylist() *jsonPlaylist {
+	return &jsonPlaylist{
+		name:     fmt.Sprintf("playlist_%d.json", time.Now().Unix()),
+		Segments: make(map[string][]jsonSeg),
+	}
+}
+
+func (jpl *jsonPlaylist) InsertHLSSegment(profile *ffmpeg.VideoProfile, seqNo uint64, uri string,
+	duration float64) {
+
+	durationMs := uint64(duration * 1000)
+	if profile.Name == "source" {
+		jpl.DurationMs += durationMs
+	}
+	jpl.Segments[profile.Name] = append(jpl.Segments[profile.Name], jsonSeg{
+		URI:        uri,
+		DurationMS: durationMs,
+		SeqNo:      seqNo,
+	})
 }
 
 // NewBasicPlaylistManager create new BasicPlaylistManager struct
@@ -51,6 +89,9 @@ func NewBasicPlaylistManager(manifestID ManifestID,
 		masterPList:    m3u8.NewMasterPlaylist(),
 		mediaLists:     make(map[string]*m3u8.MediaPlaylist),
 		mapSync:        &sync.RWMutex{},
+	}
+	if recordSession != nil {
+		bplm.jsonList = newJSONPlaylist()
 	}
 	return bplm
 }
@@ -69,6 +110,17 @@ func (mgr *BasicPlaylistManager) GetOSSession() drivers.OSSession {
 
 func (mgr *BasicPlaylistManager) GetRecordOSSession() drivers.OSSession {
 	return mgr.recordSession
+}
+
+func (mgr *BasicPlaylistManager) FlushRecord() {
+	if mgr.recordSession != nil {
+		b, err := json.Marshal(mgr.jsonList)
+		if err != nil {
+			glog.Error("Error encoding playlist: ", err)
+			return
+		}
+		mgr.recordSession.SaveData(mgr.jsonList.name, b, nil)
+	}
 }
 
 func (mgr *BasicPlaylistManager) getPL(rendition string) *m3u8.MediaPlaylist {
@@ -109,6 +161,9 @@ func (mgr *BasicPlaylistManager) InsertHLSSegment(profile *ffmpeg.VideoProfile, 
 	}
 	if mpl.Count() == 0 {
 		mpl.SeqNo = mseg.SeqId
+	}
+	if mgr.jsonList != nil {
+		mgr.jsonList.InsertHLSSegment(profile, seqNo, uri, duration)
 	}
 
 	return mpl.InsertSegment(seqNo, mseg)
