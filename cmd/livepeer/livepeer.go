55d90f0214c93de25390825457a6a85b381f468a
Ivan Tivonenko <darkdarkdragon@gmail.com>
2020-08-01 18:13:44 +0300

    Always initialise NodeID

--- cmd/livepeer/livepeer.go
+++ cmd/livepeer/livepeer.go
@@ -304,13 +304,15 @@ func main() {
 		glog.Fatalf("No services enabled; must be at least one of -broadcaster, -transcoder, -orchestrator, -redeemer, -reward or -initializeRound")
 	}
 
+	lpmon.NodeID = *ethAcctAddr
+	if lpmon.NodeID != "" {
+		lpmon.NodeID += "-"
+	}
+	hn, _ := os.Hostname()
+	lpmon.NodeID += hn
+
 	if *monitor {
 		lpmon.Enabled = true
-		nodeID := *ethAcctAddr
-		if nodeID == "" {
-			hn, _ := os.Hostname()
-			nodeID = hn
-		}
 		nodeType := "dflt"
 		switch n.NodeType {
 		case core.BroadcasterNode:
@@ -322,7 +324,7 @@ func main() {
 		case core.RedeemerNode:
 			nodeType = "rdmr"
 		}
-		lpmon.InitCensus(nodeType, nodeID, core.LivepeerVersion)
+		lpmon.InitCensus(nodeType, core.LivepeerVersion)
 	}
 
 	watcherErr := make(chan error)
