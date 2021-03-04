5311358cfe7bbe15e00a50bf0778f9ef605e831a
Eli Mallon <eli@livepeer.org>
2021-03-04 13:07:32 -0800

    windows: bump to go 1.15, fully-static build

--- upload_build.sh
+++ upload_build.sh
@@ -51,12 +51,6 @@ cp $ROUTER $BASE
 # do a basic upload so we know if stuff's working prior to doing everything else
 if [[ $ARCH == "windows" ]]; then
   FILE=$BASE.zip
-  ls /mingw64/bin/
-  # This list was produced by `ldd livepeer.exe`
-  LIBS="libffi-7.dll libgcc_s_seh-1.dll libgmp-10.dll libgnutls-30.dll libhogweed-6.dll libiconv-2.dll libidn2-0.dll libintl-8.dll libnettle-8.dll libp11-kit-0.dll libtasn1-6.dll libunistring-2.dll libwinpthread-1.dll zlib1.dll"
-  for LIB in $LIBS; do
-    cp -r /mingw64/bin/$LIB ./$BASE
-  done
   zip -r ./$FILE ./$BASE
 else
   FILE=$BASE.tar.gz
