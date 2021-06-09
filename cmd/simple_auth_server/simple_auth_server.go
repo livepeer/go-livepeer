6b08393a96cdaf77f2acfd69065703277e0a3a85
Jai Luthra <me@jailuthra.in>
2021-06-09 19:00:09 +0530

    simple_auth_server: Configure detection and videoprofile

--- cmd/simple_auth_server/simple_auth_server.go
+++ cmd/simple_auth_server/simple_auth_server.go
@@ -17,6 +17,38 @@ type authWebhookReq struct {
 	Url string `json:"url"`
 }
 
+type detectClass struct {
+	ID   int    `json:"id"`
+	Name string `json: "name"`
+}
+
+type sceneClassificationProfile struct {
+	SampleRate uint          `json:"sampleRate"`
+	Classes    []detectClass `json:"classes"`
+}
+
+type detection struct {
+	Freq                       uint                       `json:"freq"`
+	SceneClassificationProfile sceneClassificationProfile `json:"sceneClassificationProfile"`
+}
+
+type profile struct {
+	Name    string `json:"name"`
+	Width   int    `json:"width"`
+	Height  int    `json:"height"`
+	Bitrate int    `json:"bitrate"`
+	FPS     uint   `json:"fps"`
+	FPSDen  uint   `json:"fpsDen"`
+	Profile string `json:"profile"`
+	GOP     string `json:"gop"`
+}
+
+type authWebhookResponse struct {
+	ManifestID string    `json:"manifestID"`
+	Profiles   []profile `json:"profiles"`
+	Detection  detection `json:"detection"`
+}
+
 func main() {
 	http.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
 		decoder := json.NewDecoder(r.Body)
@@ -40,7 +72,29 @@ func main() {
 			fmt.Printf("Detected \"fizz\" as manifestID. Crazy! Renaming to \"buzz\".\n")
 		}
 		fmt.Printf("Stream started with manifestID: %v\n", mid)
-		w.Write([]byte(fmt.Sprintf("{\"ManifestID\":\"%v\"}", mid)))
+
+		resp := authWebhookResponse{
+			ManifestID: string(mid),
+			Profiles: []profile{{
+				Name:    "240p",
+				Width:   426,
+				Height:  240,
+				Bitrate: 250000,
+				FPS:     0,
+			}},
+			Detection: detection{
+				Freq: 4,
+				SceneClassificationProfile: sceneClassificationProfile{
+					SampleRate: 1,
+					Classes: []detectClass{
+						{ID: 0, Name: "adult"},
+						{ID: 1, Name: "soccer"},
+					},
+				},
+			},
+		}
+		byteSlice, _ := json.Marshal(resp)
+		w.Write(byteSlice)
 	})
 
 	fmt.Println("Listening on localhost:8000/auth\nTry something crazy - stream with \"fizz\" as the manifestID.")
