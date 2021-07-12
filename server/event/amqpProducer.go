d7ff2bc5581b26707c993fb7fa3ed9644e87b2e9
Victor Elias <victorgelias@gmail.com>
2021-07-12 15:34:31 -0300

    server/event: Abstract amqpProducer for testing

--- server/event/amqpProducer.go
+++ server/event/amqpProducer.go
@@ -27,20 +27,32 @@ type publishMessage struct {
 	retries int
 }
 
+type amqpChan interface {
+	Publish(exchange, key string, mandatory, immediate bool, msg amqp.Publishing) error
+}
+
+type connectFunc func(ctx context.Context, uri, exchange string, confirms chan amqp.Confirmation, closed chan *amqp.Error) (amqpChan, error)
+
 type amqpProducer struct {
 	ctx             context.Context
 	amqpURI         string
 	exchange, keyNs string
 	publishQ        chan *publishMessage
+	connectFn       connectFunc
 }
 
 func NewAMQPProducer(ctx context.Context, uri, exchange, keyNs string) Producer {
+	return newAMQPProducerInternal(ctx, uri, exchange, keyNs, amqpConnect)
+}
+
+func newAMQPProducerInternal(ctx context.Context, uri, exchange, keyNs string, connectFn connectFunc) Producer {
 	amqp := &amqpProducer{
-		ctx:      ctx,
-		amqpURI:  uri,
-		exchange: exchange,
-		keyNs:    keyNs,
-		publishQ: make(chan *publishMessage, PublishQueueSize),
+		ctx:       ctx,
+		amqpURI:   uri,
+		exchange:  exchange,
+		keyNs:     keyNs,
+		publishQ:  make(chan *publishMessage, PublishQueueSize),
+		connectFn: connectFn,
 	}
 	go amqp.mainLoop()
 	return amqp
@@ -98,14 +110,16 @@ func (p *amqpProducer) mainLoop() {
 }
 
 func (p *amqpProducer) connectAndLoopPublish() error {
-	conn, channel, err := p.setupConnection()
+	var (
+		ctx, cancel = context.WithCancel(p.ctx)
+		confirms    = make(chan amqp.Confirmation, PublishQueueSize)
+		closed      = make(chan *amqp.Error, 1)
+	)
+	defer cancel()
+	channel, err := p.connectFn(ctx, p.amqpURI, p.exchange, confirms, closed)
 	if err != nil {
 		return fmt.Errorf("error setting up AMQP connection: %w", err)
 	}
-	defer conn.Close()
-
-	closed := channel.NotifyClose(make(chan *amqp.Error, 1))
-	confirms := channel.NotifyPublish(make(chan amqp.Confirmation, PublishQueueSize))
 
 	nextMsgTag := uint64(1)
 	outstandingMsgs := map[uint64]*publishMessage{}
@@ -169,20 +183,26 @@ func (p *amqpProducer) retryMsg(msg *publishMessage) {
 	}
 }
 
-func (p *amqpProducer) setupConnection() (*amqp.Connection, *amqp.Channel, error) {
-	conn, err := amqp.Dial(p.amqpURI)
+func amqpConnect(ctx context.Context, uri, exchange string,
+	confirms chan amqp.Confirmation, closed chan *amqp.Error) (amqpChan, error) {
+
+	conn, err := amqp.Dial(uri)
 	if err != nil {
-		return nil, nil, fmt.Errorf("dial: %w", err)
+		return nil, fmt.Errorf("dial: %w", err)
 	}
+	go func() {
+		<-ctx.Done()
+		conn.Close()
+	}()
 
 	channel, err := conn.Channel()
 	if err != nil {
 		conn.Close()
-		return nil, nil, fmt.Errorf("open channel: %w", err)
+		return nil, fmt.Errorf("open channel: %w", err)
 	}
 	if err := channel.Confirm(false); err != nil {
 		conn.Close()
-		return nil, nil, fmt.Errorf("request confirms: %w", err)
+		return nil, fmt.Errorf("request confirms: %w", err)
 	}
 
 	var (
@@ -191,10 +211,12 @@ func (p *amqpProducer) setupConnection() (*amqp.Connection, *amqp.Channel, error
 		internal    = false
 		noWait      = false
 	)
-	err = channel.ExchangeDeclare(p.exchange, "topic", durable, autoDeleted, internal, noWait, nil)
+	err = channel.ExchangeDeclare(exchange, "topic", durable, autoDeleted, internal, noWait, nil)
 	if err != nil {
 		conn.Close()
-		return nil, nil, fmt.Errorf("exchange declare: %w", err)
+		return nil, fmt.Errorf("exchange declare: %w", err)
 	}
-	return conn, channel, nil
+	channel.NotifyPublish(confirms)
+	channel.NotifyClose(closed)
+	return channel, nil
 }
