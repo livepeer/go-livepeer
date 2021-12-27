10fd20187006a4d47177fae8dbdfcae42e0b4422
Yondon Fu <yondon.fu@gmail.com>
2021-12-27 08:35:52 -0500

    server: Support EIP-712 signing via /signMessage

--- server/handlers_test.go
+++ server/handlers_test.go
@@ -684,6 +684,96 @@ func TestSignMessageHandler(t *testing.T) {
 	body, _ = ioutil.ReadAll(resp.Body)
 	assert.Equal(http.StatusOK, resp.StatusCode)
 	assert.Equal([]byte(msg), body)
+
+	// Test signing typed data JSON unmarshal error
+	headers := map[string]string{
+		"Content-Type": "application/x-www-form-urlencoded",
+		"SigFormat":    "data/typed",
+	}
+
+	resp = httpPostResp(handler, strings.NewReader(form.Encode()), headers)
+	defer resp.Body.Close()
+	body, _ = ioutil.ReadAll(resp.Body)
+	assert.Equal(http.StatusBadRequest, resp.StatusCode)
+	assert.Contains(strings.TrimSpace(string(body)), "could not unmarshal typed data")
+
+	// Test signing typed data success
+	jsonTypedData := `
+    {
+      "types": {
+        "EIP712Domain": [
+          {
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "name": "version",
+            "type": "string"
+          },
+          {
+            "name": "chainId",
+            "type": "uint256"
+          },
+          {
+            "name": "verifyingContract",
+            "type": "address"
+          }
+        ],
+        "Person": [
+          {
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "name": "wallet",
+            "type": "address"
+          }
+        ],
+        "Mail": [
+          {
+            "name": "from",
+            "type": "Person"
+          },
+          {
+            "name": "to",
+            "type": "Person"
+          },
+          {
+            "name": "contents",
+            "type": "string"
+          }
+        ]
+      },
+      "primaryType": "Mail",
+      "domain": {
+        "name": "Ether Mail",
+        "version": "1",
+        "chainId": "1",
+        "verifyingContract": "0xCCCcccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC"
+      },
+      "message": {
+        "from": {
+          "name": "Cow",
+          "wallet": "0xcD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826"
+        },
+        "to": {
+          "name": "Bob",
+          "wallet": "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"
+        },
+        "contents": "Hello, Bob!"
+      }
+    }
+	`
+
+	form = url.Values{
+		"message": {jsonTypedData},
+	}
+
+	resp = httpPostResp(handler, strings.NewReader(form.Encode()), headers)
+	defer resp.Body.Close()
+	body, _ = ioutil.ReadAll(resp.Body)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	assert.Equal([]byte("foo"), body)
 }
 
 func TestVoteHandler(t *testing.T) {
