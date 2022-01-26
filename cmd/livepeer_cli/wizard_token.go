e439152714ef5959332e955756cccc9afd7ef73d
noisersup <patryk@kwiatek.xyz>
2022-01-26 05:53:48 +0100

    Y/N Confirmation when sending a transaction via the CLI (#2212)

--- cmd/livepeer_cli/wizard_token.go
+++ cmd/livepeer_cli/wizard_token.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/url"
+	"strings"
 )
 
 func (w *wizard) transferTokens() {
@@ -17,6 +19,27 @@ func (w *wizard) transferTokens() {
 	val := url.Values{
 		"to":     {fmt.Sprintf("%v", to)},
 		"amount": {fmt.Sprintf("%v", amount.String())},
+	}
+
+	var answer string
+	userAccepted := false
+	for !userAccepted {
+		fmt.Printf("Are you sure you want to send %s LPT to \"%s\"? [y/n]: ", val["amount"], val["to"])
+
+		_, err := fmt.Scanln(&answer)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch strings.ToLower(answer) {
+		case "y", "yes":
+			userAccepted = true
+		case "n", "no":
+			fmt.Println("Transaction cancelled: Interrupted by user.")
+			return
+		default:
+			fmt.Println("Wrong input!")
+		}
 	}
 
 	httpPostWithParams(fmt.Sprintf("http://%v:%v/transferTokens", w.host, w.httpPort), val)
