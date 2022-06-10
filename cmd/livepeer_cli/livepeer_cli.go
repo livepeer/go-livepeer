783bfdb392b114f8e5ddfcc95624ededaea5788d
Thom Shutt <thomshutt@users.noreply.github.com>
2022-06-10 19:35:23 +0100

    cli: Show O rather than B options when -redeemer flag set

--- cmd/livepeer_cli/livepeer_cli.go
+++ cmd/livepeer_cli/livepeer_cli.go
@@ -120,12 +120,14 @@ func (w *wizard) initializeOptions() []wizardOpt {
 }
 
 func (w *wizard) filterOptions(options []wizardOpt) []wizardOpt {
+	isOrchestratorOrRedeemer := w.orchestrator || w.isRedeemer()
+	println("isOrchestratorOrRedeemer", isOrchestratorOrRedeemer)
 	filtered := make([]wizardOpt, 0, len(options))
 	for _, opt := range options {
 		if opt.testnet && !w.testnet {
 			continue
 		}
-		if !opt.orchestrator && !opt.notOrchestrator || w.orchestrator && opt.orchestrator || !w.orchestrator && opt.notOrchestrator {
+		if !opt.orchestrator && !opt.notOrchestrator || isOrchestratorOrRedeemer && opt.orchestrator || !isOrchestratorOrRedeemer && opt.notOrchestrator {
 			filtered = append(filtered, opt)
 		}
 	}
--- server/handlers.go
+++ server/handlers.go
@@ -3,6 +3,14 @@ package server
 import (
 	"encoding/json"
 	"fmt"
+	"math/big"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/cenkalti/backoff"
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
@@ -15,13 +23,6 @@ import (
 	"github.com/livepeer/go-livepeer/pm"
 	"github.com/livepeer/lpms/ffmpeg"
 	"github.com/pkg/errors"
-	"math/big"
-	"net/http"
-	"net/url"
-	"regexp"
-	"strconv"
-	"strings"
-	"time"
 )
 
 const MainnetChainId = 1
@@ -110,6 +111,12 @@ func (s *LivepeerServer) orchestratorInfoHandler(client eth.LivepeerEthClient) h
 func (s *LivepeerServer) isOrchestratorHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		respondOk(w, []byte(fmt.Sprintf("%v", s.LivepeerNode.NodeType == core.OrchestratorNode)))
+	})
+}
+
+func (s *LivepeerServer) isRedeemerHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		respondOk(w, []byte(fmt.Sprintf("%v", s.LivepeerNode.NodeType == core.RedeemerNode)))
 	})
 }
 
