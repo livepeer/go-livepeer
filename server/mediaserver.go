f7a3402306486d484b64c036f6e7010eff9abd2d
gpu <oscar_davids@outlook.com>
2022-09-19 20:19:39 +0800

    monitoring when the session exist

--- server/mediaserver.go
+++ server/mediaserver.go
@@ -824,7 +824,7 @@ func (s *LivepeerServer) HandlePush(w http.ResponseWriter, r *http.Request) {
 		mid = intmid
 	}
 	cxn, exists := s.getActiveRtmpConnectionUnsafe(mid)
-	if monitor.Enabled && exists {
+	if monitor.Enabled && exists && cxn != nil {
 		fastVerificationEnabled, fastVerificationUsing := countStreamsWithFastVerificationEnabled(s.rtmpConnections)
 		monitor.FastVerificationEnabledAndUsingCurrentSessions(fastVerificationEnabled, fastVerificationUsing)
 	}
