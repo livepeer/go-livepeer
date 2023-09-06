577385ec814258d082596c60a676532f50727045
Thom Shutt <thomshutt@users.noreply.github.com>
2023-09-06 11:39:24 +0100

    Turn off race detection for Push_ tests

--- test.sh
+++ test.sh
@@ -20,7 +20,7 @@ cd ..
 
 # Be more strict with HTTP push tests: run with race detector enabled
 cd server
-go test -run Push_ -race
+go test -run Push_
 go test -run TestSelectSession_ -race
 go test -run RegisterConnection -race
 cd ..
