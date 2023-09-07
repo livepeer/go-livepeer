27e68850e430b12601c498c2a6f3cb022017b7d5
Thom Shutt <thomshutt@users.noreply.github.com>
2023-09-07 14:38:41 +0100

    Don't run Push_ tests twice

--- test.sh
+++ test.sh
@@ -20,7 +20,6 @@ cd ..
 
 # Be more strict with HTTP push tests: run with race detector enabled
 cd server
-go test -run Push_
 go test -run TestSelectSession_ -race
 go test -run RegisterConnection -race
 cd ..
