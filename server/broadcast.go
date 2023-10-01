f2b94ca3234f3864e008756db3704978d9c69d90
0xb79orch <0xb79orch@gmail.com>
2023-10-01 16:35:51 +0000

    Broadcaster Introspection: add ethaddress and orchestrator key data to performance selection

--- server/broadcast.go
+++ server/broadcast.go
@@ -292,6 +292,8 @@ func selectSession(ctx context.Context, sessions []*BroadcastSession, exclude []
 		// threshold is selectable
 		if len(session.SegsInFlight) == 0 {
 			if session.LatencyScore > 0 && session.LatencyScore <= SELECTOR_LATENCY_SCORE_THRESHOLD {
+				ctx = clog.AddVal(ctx, "ethaddress", session.RecipientAddress())
+				ctx = clog.AddVal(ctx, "orchestrator", session.Transcoder())
 				clog.PublicInfof(ctx,
 					"Reusing Orchestrator, reason=%v",
 					fmt.Sprintf(
@@ -329,6 +331,8 @@ func selectSession(ctx context.Context, sessions []*BroadcastSession, exclude []
 			}
 
 			if timeInFlight < maxTimeInFlight {
+				ctx = clog.AddVal(ctx, "ethaddress", session.RecipientAddress())
+				ctx = clog.AddVal(ctx, "orchestrator", session.Transcoder())
 				clog.PublicInfof(ctx,
 					"Reusing orchestrator reason=%v",
 					fmt.Sprintf(
