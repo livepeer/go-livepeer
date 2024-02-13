04c751cb7165a87b7de9f234b0da253b294eb871
Yondon Fu <yondon.fu@gmail.com>
2024-02-13 03:30:47 +0000

    multi: Support specifying external containers

--- core/ai.go
+++ core/ai.go
@@ -2,6 +2,9 @@ package core
 
 import (
 	"context"
+	"encoding/json"
+	"os"
+	"strings"
 
 	"github.com/livepeer/ai-worker/worker"
 )
@@ -10,6 +13,41 @@ type AI interface {
 	TextToImage(context.Context, worker.TextToImageJSONRequestBody) (*worker.ImageResponse, error)
 	ImageToImage(context.Context, worker.ImageToImageMultipartRequestBody) (*worker.ImageResponse, error)
 	ImageToVideo(context.Context, worker.ImageToVideoMultipartRequestBody) (*worker.VideoResponse, error)
-	Warm(context.Context, string, string) error
+	Warm(context.Context, string, string, string) error
 	Stop(context.Context) error
+}
+
+type AIModelConfig struct {
+	Pipeline string `json:"pipeline"`
+	ModelID  string `json:"model_id"`
+	Endpoint string `json:"endpoint,omitempty"`
+}
+
+func ParseAIModelConfigs(config string) ([]AIModelConfig, error) {
+	var configs []AIModelConfig
+
+	info, err := os.Stat(config)
+	if err == nil && !info.IsDir() {
+		data, err := os.ReadFile(config)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := json.Unmarshal(data, &configs); err != nil {
+			return nil, err
+		}
+
+		return configs, nil
+	}
+
+	models := strings.Split(config, ",")
+	for _, m := range models {
+		parts := strings.Split(m, ":")
+		pipeline := parts[0]
+		modelID := parts[1]
+
+		configs = append(configs, AIModelConfig{Pipeline: pipeline, ModelID: modelID})
+	}
+
+	return configs, nil
 }
