a77f0a0cf1e623f54e76beb28e21f714fc0ccdb5
Yondon Fu <yondon.fu@gmail.com>
2024-03-14 20:13:04 +0000

    server: Payments + session manager for all AI caps

--- server/ai_process.go
+++ server/ai_process.go
@@ -6,18 +6,17 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"image"
 	"io"
 	"math/big"
 	"net/http"
 	"path/filepath"
-	"sort"
 	"strings"
 
 	"github.com/livepeer/ai-worker/worker"
 	"github.com/livepeer/go-livepeer/clog"
 	"github.com/livepeer/go-livepeer/common"
 	"github.com/livepeer/go-livepeer/core"
-	"github.com/livepeer/go-livepeer/net"
 	"github.com/livepeer/go-tools/drivers"
 	"github.com/livepeer/lpms/stream"
 )
@@ -41,84 +40,10 @@ type aiRequestParams struct {
 	sessManager *AISessionManager
 }
 
-func getOrchestratorsForAIRequest(ctx context.Context, params aiRequestParams, cap core.Capability, modelID string) ([]*net.OrchestratorInfo, error) {
-	// No warm constraints applied here because we don't want to filter out orchs based on warm criteria at discovery time
-	// Instead, we want all orchs that support the model and then will prioritize orchs that have a warm model at selection time
-	constraints := map[core.Capability]*core.Constraints{
-		cap: {
-			Models: map[string]*core.ModelConstraint{
-				modelID: {
-					Warm: false,
-				},
-			},
-		},
-	}
-	caps := core.NewCapabilitiesWithConstraints(append(core.DefaultCapabilities(), cap), nil, constraints)
-
-	// Set numOrchs to the pool size so that discovery tries to find maximum # of compatible orchs within a timeout
-	numOrchs := params.node.OrchestratorPool.Size()
-	orchDesc, err := params.node.OrchestratorPool.GetOrchestrators(ctx, numOrchs, newSuspender(), caps, common.ScoreAtLeast(0))
+func processTextToImage(ctx context.Context, params aiRequestParams, req worker.TextToImageJSONRequestBody) (*worker.ImageResponse, error) {
+	resp, err := processAIRequest(ctx, params, req)
 	if err != nil {
 		return nil, err
-	}
-
-	orchInfos := orchDesc.GetRemoteInfos()
-
-	// Sort and prioritize orchs that have warm model
-	sort.Slice(orchInfos, func(i, j int) bool {
-		iConstraints, ok := orchInfos[i].Capabilities.Constraints[uint32(cap)]
-		if !ok {
-			return false
-		}
-
-		iModelConstraint, ok := iConstraints.Models[modelID]
-		if !ok {
-			return false
-		}
-
-		// If warm i goes before j, else j goes before i
-		return iModelConstraint.Warm
-	})
-
-	return orchInfos, nil
-}
-
-func processTextToImage(ctx context.Context, params aiRequestParams, req worker.TextToImageJSONRequestBody) (*worker.ImageResponse, error) {
-	modelID := defaultTextToImageModelID
-	if req.ModelId != nil {
-		modelID = *req.ModelId
-	}
-
-	var resp *worker.ImageResponse
-
-	tries := 0
-	for tries < maxProcessingRetries {
-		tries++
-
-		sess, err := params.sessManager.Select(ctx, core.Capability_TextToImage, modelID)
-		if err != nil {
-			clog.Infof(ctx, "Error selecting TextToImage session err=%v", err)
-			continue
-		}
-
-		if sess == nil {
-			break
-		}
-
-		resp, err = submitTextToImage(ctx, params, sess, req)
-		if err == nil {
-			params.sessManager.Complete(ctx, sess)
-			break
-		}
-
-		clog.Infof(ctx, "Error submitting TextToImage request try=%v orch=%v err=%v", tries, sess.Transcoder(), err)
-
-		params.sessManager.Remove(ctx, sess)
-
-	}
-
-	if resp == nil {
-		return nil, &ServiceUnavailableError{err: errors.New("no orchestrators available")}
 	}
 
 	newMedia := make([]worker.Media, len(resp.Images))
@@ -148,17 +73,6 @@ func submitTextToImage(ctx context.Context, params aiRequestParams, sess *AISess
 		return nil, err
 	}
 
-	// genSegCreds expects a stream.HLSSegment so in order to reuse it here we pass a dummy object
-	segCreds, err := genSegCreds(sess.BroadcastSession, &stream.HLSSegment{}, nil, false)
-	if err != nil {
-		return nil, err
-	}
-
-	priceInfo, err := common.RatPriceInfo(sess.OrchestratorInfo.GetPriceInfo())
-	if err != nil {
-		return nil, err
-	}
-
 	if req.Height == nil {
 		req.Height = new(int)
 		*req.Height = 512
@@ -168,43 +82,16 @@ func submitTextToImage(ctx context.Context, params aiRequestParams, sess *AISess
 		*req.Width = 512
 	}
 
-	// If the # of inference/denoising steps becomes configurable, a possible updated formula could be height * width * steps
-	// If additional parameters that influence compute cost become configurable, then the formula should be reconsidered
-	outPixels := int64(*req.Height) * int64(*req.Height)
-	fee, err := estimateAIFee(outPixels, priceInfo)
-	if err != nil {
-		return nil, err
-	}
-
-	balUpdate, err := newBalanceUpdate(sess.BroadcastSession, fee)
+	outPixels := int64(*req.Height) * int64(*req.Width)
+	setHeaders, balUpdate, err := prepareAIPayment(ctx, sess, outPixels)
 	if err != nil {
 		return nil, err
 	}
 	defer completeBalanceUpdate(sess.BroadcastSession, balUpdate)
 
-	payment, err := genPayment(ctx, sess.BroadcastSession, balUpdate.NumTickets)
-	if err != nil {
-		return nil, err
-	}
-
-	// As soon as the request is sent to the orch consider the balance update's credit as spent
-	balUpdate.Status = CreditSpent
-
-	setHeaders := func(_ context.Context, req *http.Request) error {
-		req.Header.Set(segmentHeader, segCreds)
-		req.Header.Set(paymentHeader, payment)
-		return nil
-	}
-
 	resp, err := client.TextToImageWithResponse(ctx, req, setHeaders)
 	if err != nil {
 		return nil, err
-	}
-
-	// We treat a response as "receiving change" where the change is the difference between the credit and debit for the update
-	balUpdate.Status = ReceivedChange
-	if priceInfo != nil {
-		balUpdate.Debit = fee
 	}
 
 	if resp.JSON200 == nil {
@@ -212,50 +99,18 @@ func submitTextToImage(ctx context.Context, params aiRequestParams, sess *AISess
 		return nil, errors.New(strings.TrimSuffix(string(resp.Body), "\n"))
 	}
 
+	// We treat a response as "receiving change" where the change is the difference between the credit and debit for the update
+	if balUpdate != nil {
+		balUpdate.Status = ReceivedChange
+	}
+
 	return resp.JSON200, nil
 }
 
 func processImageToImage(ctx context.Context, params aiRequestParams, req worker.ImageToImageMultipartRequestBody) (*worker.ImageResponse, error) {
-	modelID := defaultImageToImageModelID
-	if req.ModelId != nil {
-		modelID = *req.ModelId
-	}
-
-	orchInfos, err := getOrchestratorsForAIRequest(ctx, params, core.Capability_ImageToImage, modelID)
+	resp, err := processAIRequest(ctx, params, req)
 	if err != nil {
 		return nil, err
-	}
-
-	if len(orchInfos) == 0 {
-		return nil, &ServiceUnavailableError{err: errors.New("no orchestrators available")}
-	}
-
-	var resp *worker.ImageResponse
-
-	// Round robin up to maxProcessingRetries times
-	orchIdx := 0
-	tries := 0
-	for tries < maxProcessingRetries {
-		orchUrl := orchInfos[orchIdx].Transcoder
-
-		var err error
-		resp, err = submitImageToImage(ctx, orchUrl, req)
-		if err == nil {
-			break
-		}
-
-		clog.Infof(ctx, "Error submitting ImageToImage request try=%v orch=%v err=%v", tries, orchUrl, err)
-
-		tries++
-		orchIdx++
-		// Wrap back around
-		if orchIdx >= len(orchInfos) {
-			orchIdx = 0
-		}
-	}
-
-	if resp == nil {
-		return nil, &ServiceUnavailableError{err: errors.New("no orchestrators available")}
 	}
 
 	newMedia := make([]worker.Media, len(resp.Images))
@@ -279,19 +134,35 @@ func processImageToImage(ctx context.Context, params aiRequestParams, req worker
 	return resp, nil
 }
 
-func submitImageToImage(ctx context.Context, url string, req worker.ImageToImageMultipartRequestBody) (*worker.ImageResponse, error) {
+func submitImageToImage(ctx context.Context, params aiRequestParams, sess *AISession, req worker.ImageToImageMultipartRequestBody) (*worker.ImageResponse, error) {
 	var buf bytes.Buffer
 	mw, err := worker.NewImageToImageMultipartWriter(&buf, req)
 	if err != nil {
 		return nil, err
 	}
 
-	client, err := worker.NewClientWithResponses(url, worker.WithHTTPClient(httpClient))
+	client, err := worker.NewClientWithResponses(sess.Transcoder(), worker.WithHTTPClient(httpClient))
 	if err != nil {
 		return nil, err
 	}
 
-	resp, err := client.ImageToImageWithBodyWithResponse(ctx, mw.FormDataContentType(), &buf)
+	imageRdr, err := req.Image.Reader()
+	if err != nil {
+		return nil, err
+	}
+	config, _, err := image.DecodeConfig(imageRdr)
+	if err != nil {
+		return nil, err
+	}
+	outPixels := int64(config.Height) * int64(config.Width)
+
+	setHeaders, balUpdate, err := prepareAIPayment(ctx, sess, outPixels)
+	if err != nil {
+		return nil, err
+	}
+	defer completeBalanceUpdate(sess.BroadcastSession, balUpdate)
+
+	resp, err := client.ImageToImageWithBodyWithResponse(ctx, mw.FormDataContentType(), &buf, setHeaders)
 	if err != nil {
 		return nil, err
 	}
@@ -301,50 +172,18 @@ func submitImageToImage(ctx context.Context, url string, req worker.ImageToImage
 		return nil, errors.New(strings.TrimSuffix(string(resp.Body), "\n"))
 	}
 
+	// We treat a response as "receiving change" where the change is the difference between the credit and debit for the update
+	if balUpdate != nil {
+		balUpdate.Status = ReceivedChange
+	}
+
 	return resp.JSON200, nil
 }
 
 func processImageToVideo(ctx context.Context, params aiRequestParams, req worker.ImageToVideoMultipartRequestBody) (*worker.ImageResponse, error) {
-	modelID := defaultImageToVideoModelID
-	if req.ModelId != nil {
-		modelID = *req.ModelId
-	}
-
-	orchInfos, err := getOrchestratorsForAIRequest(ctx, params, core.Capability_ImageToVideo, modelID)
+	resp, err := processAIRequest(ctx, params, req)
 	if err != nil {
 		return nil, err
-	}
-
-	if len(orchInfos) == 0 {
-		return nil, &ServiceUnavailableError{err: errors.New("no orchestrators available")}
-	}
-
-	var resp *worker.ImageResponse
-
-	// Round robin up to maxProcessingRetries times
-	orchIdx := 0
-	tries := 0
-	for tries < maxProcessingRetries {
-		orchUrl := orchInfos[orchIdx].Transcoder
-
-		var err error
-		resp, err = submitImageToVideo(ctx, orchUrl, req)
-		if err == nil {
-			break
-		}
-
-		clog.Infof(ctx, "Error submitting ImageToVideo request try=%v orch=%v err=%v", tries, orchUrl, err)
-
-		tries++
-		orchIdx++
-		// Wrap back around
-		if orchIdx >= len(orchInfos) {
-			orchIdx = 0
-		}
-	}
-
-	if resp == nil {
-		return nil, &ServiceUnavailableError{err: errors.New("no orchestrators available")}
 	}
 
 	// HACK: Re-use worker.ImageResponse to return results
@@ -372,19 +211,35 @@ func processImageToVideo(ctx context.Context, params aiRequestParams, req worker
 	return resp, nil
 }
 
-func submitImageToVideo(ctx context.Context, url string, req worker.ImageToVideoMultipartRequestBody) (*worker.ImageResponse, error) {
+func submitImageToVideo(ctx context.Context, params aiRequestParams, sess *AISession, req worker.ImageToVideoMultipartRequestBody) (*worker.ImageResponse, error) {
 	var buf bytes.Buffer
 	mw, err := worker.NewImageToVideoMultipartWriter(&buf, req)
 	if err != nil {
 		return nil, err
 	}
 
-	client, err := worker.NewClientWithResponses(url, worker.WithHTTPClient(httpClient))
+	client, err := worker.NewClientWithResponses(sess.Transcoder(), worker.WithHTTPClient(httpClient))
 	if err != nil {
 		return nil, err
 	}
 
-	resp, err := client.ImageToVideoWithBody(ctx, mw.FormDataContentType(), &buf)
+	if req.Height == nil {
+		req.Height = new(int)
+		*req.Height = 576
+	}
+	if req.Width == nil {
+		req.Width = new(int)
+		*req.Width = 1024
+	}
+
+	outPixels := int64(*req.Height) * int64(*req.Width)
+	setHeaders, balUpdate, err := prepareAIPayment(ctx, sess, outPixels)
+	if err != nil {
+		return nil, err
+	}
+	defer completeBalanceUpdate(sess.BroadcastSession, balUpdate)
+
+	resp, err := client.ImageToVideoWithBody(ctx, mw.FormDataContentType(), &buf, setHeaders)
 	if err != nil {
 		return nil, err
 	}
@@ -399,12 +254,131 @@ func submitImageToVideo(ctx context.Context, url string, req worker.ImageToVideo
 		return nil, errors.New(string(data))
 	}
 
+	// We treat a response as "receiving change" where the change is the difference between the credit and debit for the update
+	if balUpdate != nil {
+		balUpdate.Status = ReceivedChange
+	}
+
 	var res worker.ImageResponse
 	if err := json.Unmarshal(data, &res); err != nil {
 		return nil, err
 	}
 
 	return &res, nil
+}
+
+func processAIRequest(ctx context.Context, params aiRequestParams, req interface{}) (*worker.ImageResponse, error) {
+	var cap core.Capability
+	var modelID string
+	var submitFn func(context.Context, aiRequestParams, *AISession) (*worker.ImageResponse, error)
+
+	switch v := req.(type) {
+	case worker.TextToImageJSONRequestBody:
+		cap = core.Capability_TextToImage
+		modelID = defaultTextToImageModelID
+		if v.ModelId != nil {
+			modelID = *v.ModelId
+		}
+		submitFn = func(ctx context.Context, params aiRequestParams, sess *AISession) (*worker.ImageResponse, error) {
+			return submitTextToImage(ctx, params, sess, v)
+		}
+	case worker.ImageToImageMultipartRequestBody:
+		cap = core.Capability_ImageToImage
+		modelID = defaultImageToImageModelID
+		if v.ModelId != nil {
+			modelID = *v.ModelId
+		}
+		submitFn = func(ctx context.Context, params aiRequestParams, sess *AISession) (*worker.ImageResponse, error) {
+			return submitImageToImage(ctx, params, sess, v)
+		}
+	case worker.ImageToVideoMultipartRequestBody:
+		cap = core.Capability_ImageToVideo
+		modelID = defaultImageToVideoModelID
+		if v.ModelId != nil {
+			modelID = *v.ModelId
+		}
+		submitFn = func(ctx context.Context, params aiRequestParams, sess *AISession) (*worker.ImageResponse, error) {
+			return submitImageToVideo(ctx, params, sess, v)
+		}
+	default:
+		return nil, errors.New("unknown AI request type")
+	}
+
+	var resp *worker.ImageResponse
+
+	tries := 0
+	for tries < maxProcessingRetries {
+		tries++
+
+		sess, err := params.sessManager.Select(ctx, cap, modelID)
+		if err != nil {
+			clog.Infof(ctx, "Error selecting session cap=%v modelID=%v err=%v", cap, modelID, err)
+			continue
+		}
+
+		if sess == nil {
+			break
+		}
+
+		resp, err = submitFn(ctx, params, sess)
+		if err == nil {
+			params.sessManager.Complete(ctx, sess)
+			break
+		}
+
+		clog.Infof(ctx, "Error submitting request cap=%v modelID=%v try=%v orch=%v err=%v", cap, modelID, tries, sess.Transcoder(), err)
+
+		params.sessManager.Remove(ctx, sess)
+	}
+
+	if resp == nil {
+		return nil, &ServiceUnavailableError{err: errors.New("no orchestrators available")}
+	}
+
+	return resp, nil
+}
+
+func prepareAIPayment(ctx context.Context, sess *AISession, outPixels int64) (worker.RequestEditorFn, *BalanceUpdate, error) {
+	// genSegCreds expects a stream.HLSSegment so in order to reuse it here we pass a dummy object
+	segCreds, err := genSegCreds(sess.BroadcastSession, &stream.HLSSegment{}, nil, false)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	priceInfo, err := common.RatPriceInfo(sess.OrchestratorInfo.GetPriceInfo())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// At the moment, outPixels is expected to just be height * width
+	// If the # of inference/denoising steps becomes configurable, a possible updated formula could be height * width * steps
+	// If additional parameters that influence compute cost become configurable, then the formula should be reconsidered
+	fee, err := estimateAIFee(outPixels, priceInfo)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	balUpdate, err := newBalanceUpdate(sess.BroadcastSession, fee)
+	if err != nil {
+		return nil, nil, err
+	}
+	balUpdate.Debit = fee
+
+	payment, err := genPayment(ctx, sess.BroadcastSession, balUpdate.NumTickets)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// As soon as the request is sent to the orch consider the balance update's credit as spent
+	balUpdate.Status = CreditSpent
+
+	setHeaders := func(_ context.Context, req *http.Request) error {
+		req.Header.Set(segmentHeader, segCreds)
+		req.Header.Set(paymentHeader, payment)
+		return nil
+	}
+
+	return setHeaders, balUpdate, nil
 }
 
 func estimateAIFee(outPixels int64, priceInfo *big.Rat) (*big.Rat, error) {
