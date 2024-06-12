658859c4b3e2d9ffe8fe4c3dab979a0cabdc370e
kyriediculous <vergauwennico@gmail.com>
2024-06-12 17:50:36 +0200

    server: add aiResults route to server

--- core/ai.go
+++ core/ai.go
@@ -36,7 +36,6 @@ type RemoteAIWorkerManager struct {
 	workersMutex  sync.Mutex
 
 	// tasks
-	// TODO: how to id tasks/sessions ?
 	taskChans map[int64]RemoteAIResultChan
 	taskMutex sync.RWMutex
 	taskCount int64
