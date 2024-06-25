7302537683502763b2b73b931b9357ab6282b9ac
Rick Staa <rick.staa@outlook.com>
2024-06-25 19:41:14 +0100

    WIP: commit untested metrics for mike

--- server/ai_mediaserver.go
+++ server/ai_mediaserver.go
@@ -13,7 +13,6 @@ import (
 	"github.com/livepeer/go-livepeer/clog"
 	"github.com/livepeer/go-livepeer/common"
 	"github.com/livepeer/go-livepeer/core"
-	"github.com/livepeer/go-livepeer/monitor"
 	"github.com/livepeer/go-tools/drivers"
 	middleware "github.com/oapi-codegen/nethttp-middleware"
 	"github.com/oapi-codegen/runtime"
@@ -87,7 +86,6 @@ func (ls *LivepeerServer) TextToImage() http.Handler {
 		}
 
 		clog.V(common.VERBOSE).Infof(r.Context(), "Received TextToImage request prompt=%v model_id=%v", req.Prompt, *req.ModelId)
-		monitor.RecordModelRequested("text-to-image", *req.ModelId)
 
 		params := aiRequestParams{
 			node:        ls.LivepeerNode,
@@ -109,10 +107,6 @@ func (ls *LivepeerServer) TextToImage() http.Handler {
 
 		took := time.Since(start)
 		clog.Infof(ctx, "Processed TextToImage request prompt=%v model_id=%v took=%v", req.Prompt, *req.ModelId, took)
-
-		if monitor.Enabled {
-			monitor.AiJobProcessed(ctx, "text-to-image", *req.ModelId, took)
-		}
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
@@ -140,7 +134,6 @@ func (ls *LivepeerServer) ImageToImage() http.Handler {
 		}
 
 		clog.V(common.VERBOSE).Infof(ctx, "Received ImageToImage request imageSize=%v prompt=%v model_id=%v", req.Image.FileSize(), req.Prompt, *req.ModelId)
-		monitor.RecordModelRequested("image-to-image", *req.ModelId)
 
 		params := aiRequestParams{
 			node:        ls.LivepeerNode,
@@ -162,9 +155,6 @@ func (ls *LivepeerServer) ImageToImage() http.Handler {
 
 		took := time.Since(start)
 		clog.V(common.VERBOSE).Infof(ctx, "Processed ImageToImage request imageSize=%v prompt=%v model_id=%v took=%v", req.Image.FileSize(), req.Prompt, *req.ModelId, took)
-		if monitor.Enabled {
-			monitor.AiJobProcessed(ctx, "image-to-image", *req.ModelId, took)
-		}
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
@@ -198,7 +188,6 @@ func (ls *LivepeerServer) ImageToVideo() http.Handler {
 		}
 
 		clog.V(common.VERBOSE).Infof(ctx, "Received ImageToVideo request imageSize=%v model_id=%v async=%v", req.Image.FileSize(), *req.ModelId, async)
-		monitor.RecordModelRequested("image-to-video", *req.ModelId)
 
 		params := aiRequestParams{
 			node:        ls.LivepeerNode,
@@ -223,9 +212,7 @@ func (ls *LivepeerServer) ImageToVideo() http.Handler {
 
 			took := time.Since(start)
 			clog.Infof(ctx, "Processed ImageToVideo request imageSize=%v model_id=%v took=%v", req.Image.FileSize(), *req.ModelId, took)
-			if monitor.Enabled {
-				monitor.AiJobProcessed(ctx, "image-to-video", *req.ModelId, took)
-			}
+
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 			_ = json.NewEncoder(w).Encode(resp)
