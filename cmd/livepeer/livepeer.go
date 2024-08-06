bcde0e5c2cbbe21f5e0eff32807b6f29777d2132
Josh Allmann <joshua.allmann@gmail.com>
2024-08-06 19:58:56 +0000

    livepeer: add a `-hevcDecoding` flag
    Adds a new boolean flag `-hevcDecoding` to better configure the
    existing HEVC support.
    
    If the flag is omitted, the default behavior on GPUs is unchanged,
    which is to auto-detect HEVC decoding support at transcoder start-up.
    
    GPU transcoders can disable HEVC decoding if there is an issue with
    HEVC jobs via `-hevcDecoding=false`.
    
    CPU transcoders now have HEVC decoding disabled by default since
    processing HEVC jobs is CPU-heavy, so operators are not caught
    off-guard once we start putting HEVC jobs into the network. This is
    technically a breaking change, and some un-upgraded transcoders may
    still be surprised anyway.

--- cmd/livepeer/livepeer.go
+++ cmd/livepeer/livepeer.go
@@ -150,6 +150,7 @@ func parseLivepeerConfig() starter.LivepeerConfig {
 	cfg.Nvidia = flag.String("nvidia", *cfg.Nvidia, "Comma-separated list of Nvidia GPU device IDs (or \"all\" for all available devices)")
 	cfg.Netint = flag.String("netint", *cfg.Netint, "Comma-separated list of NetInt device GUIDs (or \"all\" for all available devices)")
 	cfg.TestTranscoder = flag.Bool("testTranscoder", *cfg.TestTranscoder, "Test Nvidia GPU transcoding at startup")
+	cfg.HevcDecoding = flag.Bool("hevcDecoding", *cfg.HevcDecoding, "Enable or disable HEVC decoding")
 
 	// Onchain:
 	cfg.EthAcctAddr = flag.String("ethAcctAddr", *cfg.EthAcctAddr, "Existing Eth account address. For use when multiple ETH accounts exist in the keystore directory")
@@ -236,6 +237,9 @@ func updateNilsForUnsetFlags(cfg starter.LivepeerConfig) starter.LivepeerConfig 
 	}
 	if !isFlagSet["localVerify"] {
 		res.LocalVerify = nil
+	}
+	if !isFlagSet["hevcDecoding"] {
+		res.HevcDecoding = nil
 	}
 
 	return res
