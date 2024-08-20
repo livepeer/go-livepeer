42b7c854df08b025daad1266123509baf92a0607
Rick Staa <rick.staa@outlook.com>
2024-08-20 13:15:31 +0200

    ci(ai): add latest tag check into build publish job
    This commit ensure the latest tag is checked in the publish job of the
    build action.

--- ci_env.sh
+++ ci_env.sh
@@ -59,9 +59,4 @@ if [[ "$CI" == "true" ]]; then
   echo "build-tags=${BUILD_TAGS}" >>"$GITHUB_OUTPUT"
 fi
 
-# Retrieve latest release tag
-git fetch --tags
-latest_tag=$(git tag -l "v*" | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+-ai.[0-9]+$' | sort -V | tail -n 1)
-echo "latest_tag=$latest_tag" >>"$GITHUB_OUTPUT"
-
 exec "$@"
