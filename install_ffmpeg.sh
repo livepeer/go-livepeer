7cc12041739af8fb09ecc0542784da4785aaa94d
Rafał Leszko <rafal@livepeer.org>
2024-10-03 13:31:58 +0200

    Update install_ffmpeg.sh

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo 'WARNING: downloading and executing lpms/install_ffmpeg.sh, use it directly in case of issues'
-curl https://raw.githubusercontent.com/livepeer/lpms/d9c78b62effdb4f5c8fc438b6033da1090d04a03/install_ffmpeg.sh | bash -s $1
+curl https://raw.githubusercontent.com/livepeer/lpms/fe5aff1fa6a21c4899f07608ea367959085f4a5c/install_ffmpeg.sh | bash -s $1
