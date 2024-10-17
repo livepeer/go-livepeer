c5c3e797a64c9372bc6b3de7498e402629e646a7
Rafał Leszko <rafal@livepeer.org>
2024-10-17 11:59:42 +0200

    Update install_ffmpeg.sh and fix cross-compilation (#3197)

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo 'WARNING: downloading and executing lpms/install_ffmpeg.sh, use it directly in case of issues'
-curl https://raw.githubusercontent.com/livepeer/lpms/d9c78b62effdb4f5c8fc438b6033da1090d04a03/install_ffmpeg.sh | bash -s $1
+curl https://raw.githubusercontent.com/livepeer/lpms/ffde2327537517b3345162e9544704571bc58a34/install_ffmpeg.sh | bash -s $1
