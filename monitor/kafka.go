d6d6006fce30daec1562c0420e4ce15dcdd532c1
gioelecerati <gioele@cerati.org>
2024-11-06 18:15:51 +0100

    send messages in batches & make send event package-private

--- monitor/kafka.go
+++ monitor/kafka.go
@@ -21,6 +21,7 @@ const (
 type KafkaProducer struct {
 	writer *kafka.Writer
 	topic  string
+	events chan GatewayEvent
 }
 
 type GatewayEvent struct {
@@ -34,15 +35,16 @@ type GatewayEvent struct {
 var kafkaProducer *KafkaProducer
 
 func InitKafkaProducer(bootstrapServers, user, password, topic string) error {
-	producer, err := NewKafkaProducer(bootstrapServers, user, password, topic)
+	producer, err := newKafkaProducer(bootstrapServers, user, password, topic)
 	if err != nil {
 		return err
 	}
 	kafkaProducer = producer
+	go producer.processEvents()
 	return nil
 }
 
-func NewKafkaProducer(bootstrapServers, user, password, topic string) (*KafkaProducer, error) {
+func newKafkaProducer(bootstrapServers, user, password, topic string) (*KafkaProducer, error) {
 	dialer := &kafka.Dialer{
 		Timeout: KafkaRequestTimeout,
 		SASLMechanism: plain.Mechanism{
@@ -66,61 +68,60 @@ func NewKafkaProducer(bootstrapServers, user, password, topic string) (*KafkaPro
 	return &KafkaProducer{
 		writer: writer,
 		topic:  topic,
+		events: make(chan GatewayEvent, 100),
 	}, nil
 }
 
-func (p *KafkaProducer) SendEvent(event GatewayEvent) {
-	p.SendEvents([]GatewayEvent{event})
-}
-
-func (p *KafkaProducer) SendEvents(events []GatewayEvent) {
-	var msgs []kafka.Message
-
-	for _, e := range events {
-		value, err := json.Marshal(e)
+func (p *KafkaProducer) processEvents() {
+	for event := range p.events {
+		value, err := json.Marshal(event)
 		if err != nil {
 			glog.Errorf("error while marshalling gateway log to Kafka, err=%v", err)
 			continue
 		}
-		msgs = append(msgs, kafka.Message{
-			Key:   []byte(*e.ID),
-			Value: value,
-		})
-	}
 
-	// We retry sending messages to Kafka in case of a failure
-	kafkaWriteRetries := 3
-	var err error
-	for i := 0; i < kafkaWriteRetries; i++ {
-		err = p.writer.WriteMessages(context.Background(), msgs...)
-		if err == nil {
-			return
-		} else {
-			glog.Warningf("error while sending gateway log to Kafka, retrying, topic=%s, try=%d, err=%v", p.topic, i, err)
+		msg := kafka.Message{
+			Key:   []byte(*event.ID),
+			Value: value,
+		}
+
+		// We retry sending messages to Kafka in case of a failure
+		kafkaWriteRetries := 3
+		var writeErr error
+		for i := 0; i < kafkaWriteRetries; i++ {
+			writeErr = p.writer.WriteMessages(context.Background(), msg)
+			if writeErr == nil {
+				break
+			}
+			glog.Warningf("error while sending gateway log to Kafka, retrying, topic=%s, try=%d, err=%v", p.topic, i, writeErr)
+		}
+		if writeErr != nil {
+			glog.Errorf("error while sending gateway log to Kafka, the gateway log is lost, err=%v", writeErr)
 		}
 	}
-	glog.Errorf("error while sending gateway log to Kafka, the gateway logs are lost, err=%d", err)
+}
+
+func (p *KafkaProducer) sendEvent(event GatewayEvent) {
+	p.events <- event
 }
 
 func SendQueueEventAsync(eventType string, data interface{}) {
-	go func() {
-		if kafkaProducer == nil {
-			return
-		}
+	if kafkaProducer == nil {
+		return
+	}
 
-		randomID := uuid.New().String()
-		timestampMs := time.Now().UnixMilli()
+	randomID := uuid.New().String()
+	timestampMs := time.Now().UnixMilli()
 
-		event := GatewayEvent{
-			ID:        stringPtr(randomID),
-			Gateway:   stringPtr(""),
-			Type:      &eventType,
-			Timestamp: stringPtr(fmt.Sprint(timestampMs)),
-			Data:      data,
-		}
+	event := GatewayEvent{
+		ID:        stringPtr(randomID),
+		Gateway:   stringPtr(""),
+		Type:      &eventType,
+		Timestamp: stringPtr(fmt.Sprint(timestampMs)),
+		Data:      data,
+	}
 
-		kafkaProducer.SendEvent(event)
-	}()
+	kafkaProducer.sendEvent(event)
 }
 
 func stringPtr(s string) *string {
