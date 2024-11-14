50bee49c2b3b1cf3cb009e8581c42f55c2be42b4
Thom Shutt <thomshutt@users.noreply.github.com>
2024-11-14 16:52:32 +0000

    Uncaught error

--- cmd/livepeer/starter/starter.go
+++ cmd/livepeer/starter/starter.go
@@ -611,7 +611,10 @@ func StartLivepeer(ctx context.Context, cfg LivepeerConfig) {
 		case core.BroadcasterNode:
 			nodeType = lpmon.Broadcaster
 			if *cfg.KafkaBootstrapServers != "" && *cfg.KafkaUsername != "" && *cfg.KafkaPassword != "" && *cfg.KafkaGatewayTopic != "" {
-				lpmon.InitKafkaProducer(*cfg.KafkaBootstrapServers, *cfg.KafkaUsername, *cfg.KafkaPassword, *cfg.KafkaGatewayTopic, n.Eth.Account().Address.Hex())
+				err := lpmon.InitKafkaProducer(*cfg.KafkaBootstrapServers, *cfg.KafkaUsername, *cfg.KafkaPassword, *cfg.KafkaGatewayTopic, n.Eth.Account().Address.Hex())
+				if err != nil {
+					glog.Warning("error while intializing Kafka producer: %w", err)
+				}
 			}
 		case core.OrchestratorNode:
 			nodeType = lpmon.Orchestrator
