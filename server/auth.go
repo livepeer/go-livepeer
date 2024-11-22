888d1529f2b5688fd170b1a03e21b5c7f58c9d33
Max Holland <max@livepeer.org>
2024-11-22 12:09:21 +0000

    Fix AI auth

--- server/auth.go
+++ server/auth.go
@@ -127,7 +127,7 @@ func authenticateAIStream(authURL *url.URL, req AIAuthRequest) error {
 
 	rbody, err := io.ReadAll(resp.Body)
 	resp.Body.Close()
-	if resp.StatusCode != 200 {
+	if resp.StatusCode < http.StatusOK || resp.StatusCode >= http.StatusMultipleChoices {
 		return fmt.Errorf("status=%d error=%s", resp.StatusCode, string(rbody))
 	}
 
