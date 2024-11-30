1d55e9bd438fe33b36a59c3d89dd67d8a4484a51
hjpotter92 <hjpotter92@users.noreply.github.com>
2024-11-30 13:07:36 +0530

    mediamtx: Publish deployed version if runtime args are present (#3285)
    * mediamtx: Publish deployed version if runtime args are present
    
    * dockerfile.mediamtx: Make use of layer caching when building docker image
    
    * mediamtx-entry.bash: Use exec to hand off the script

---
+++ docker/mediamtx-entry.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+[ -v DEBUG ] && set -x
+
+set -euo pipefail
+
+if [ -v LP_PUBLISH_MEDIAMTX_METRICS ]; then
+  if [ -z "$LP_PUBLISH_MEDIAMTX_METRICS_ENDPOINT" ]; then
+    echo >&2 "No endpoint specified for publishing mediamtx metrics."
+  fi
+  echo <<EOF |
+# HELP version Current software version as a tag, always 1
+# TYPE version gauge
+version{app="MediaMTX",version="$MEDIAMTX_VERSION"} 1
+EOF
+    curl -X POST --data-binary @- "$LP_PUBLISH_MEDIAMTX_METRICS_ENDPOINT"
+fi
+
+exec "${@:-/usr/local/bin/mediamtx}"
