61f6fb4fa145c5a8ccaa99ac067f3dd3396a4ff6
Max Holland <max@livepeer.org>
2025-02-07 14:07:49 +0000

    Add retryCount to rtmp input log line for monitoring (#3380)

--- media/rtmp2segment.go
+++ media/rtmp2segment.go
@@ -52,6 +52,7 @@ func (ms *MediaSegmenter) RunSegmentation(ctx context.Context, in string, segmen
 		processSegments(ctx, segmentHandler, outFilePattern, completionSignal)
 	}()
 
+	retryCount := 0
 	for {
 		err := backoff.Retry(func() error {
 			streamExists, err := ms.MediaMTXClient.StreamExists()
@@ -68,7 +69,7 @@ func (ms *MediaSegmenter) RunSegmentation(ctx context.Context, in string, segmen
 			clog.Errorf(ctx, "Stopping segmentation in=%s err=%s", in, err)
 			break
 		}
-		clog.Infof(ctx, "Starting segmentation. in=%s", in)
+		clog.Infof(ctx, "Starting segmentation. in=%s retryCount=%d", in, retryCount)
 		cmd := exec.CommandContext(procCtx, "ffmpeg",
 			"-i", in,
 			"-c:a", "copy",
@@ -81,8 +82,9 @@ func (ms *MediaSegmenter) RunSegmentation(ctx context.Context, in string, segmen
 			clog.Errorf(ctx, "Error receiving RTMP: %v", err)
 			break
 		}
-		clog.Infof(ctx, "Segmentation ffmpeg output: %s", output)
+		clog.Infof(ctx, "Segmentation stopped, will retry. retryCount=%d ffmpeg output: %s", retryCount, output)
 		time.Sleep(5 * time.Second)
+		retryCount++
 	}
 	completionSignal <- true
 	clog.Infof(ctx, "sent completion signal, now waiting")
