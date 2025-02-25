c3e5fa7e3ac8b69ed53b77c39d01df7881447765
Max Holland <max@livepeer.org>
2025-02-25 14:58:07 +0000

    Include orch request ID in logging

--- server/ai_http.go
+++ server/ai_http.go
@@ -101,7 +101,8 @@ func (h *lphttp) StartLiveVideoToVideo() http.Handler {
 		streamID := r.Header.Get("streamID")
 		gatewayRequestID := r.Header.Get("requestID")
 		requestID := string(core.RandomManifestID())
-		ctx = clog.AddVal(ctx, "request_id", gatewayRequestID)
+		ctx = clog.AddVal(ctx, "orch_request_id", requestID)
+		ctx = clog.AddVal(ctx, "request_id", gatewayRequestID) // use this as the common request_id across all apps for easy log filtering
 		ctx = clog.AddVal(ctx, "stream_id", streamID)
 
 		var req worker.GenLiveVideoToVideoJSONRequestBody
