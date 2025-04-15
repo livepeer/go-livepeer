c570bcf6237cebd227720050e0db8e15be976b27
Rafał Leszko <rafal@livepeer.org>
2025-04-15 10:48:05 +0200

    Fix infinite selection loop (#3507)
    * Delete inUseSess while clearing session pool
    
    * Limit the n of selection tries

--- server/selection.go
+++ server/selection.go
@@ -150,7 +150,6 @@ func (s *Selector) sort() {
 }
 
 func (s *Selector) Select(ctx context.Context) *BroadcastSession {
-	clog.V(common.DEBUG).Infof(ctx, "Selecting orchestrator")
 	availableOrchestrators := toOrchestrators(s.sessions)
 	sess := s.selectUnknownSession(ctx)
 	s.sort()
