31ff4819376a82bd295b33ed0634902445671775
Josh Allmann <joshua.allmann@gmail.com>
2025-04-16 06:51:37 +0000

    safer way to stop control channel

--- server/ai_live_video.go
+++ server/ai_live_video.go
@@ -330,15 +330,17 @@ func startControlPublish(ctx context.Context, control *url.URL, params aiRequest
 
 	ticker := time.NewTicker(10 * time.Second)
 	done := make(chan bool, 1)
+	once := sync.Once{}
 	stop := func() {
-		ticker.Stop()
-		done <- true
+		once.Do(func() {
+			ticker.Stop()
+			done <- true
+		})
 	}
 
 	if control, exists := params.node.LivePipelines[stream]; exists {
 		clog.Info(ctx, "Stopping existing control loop", "existing_request_id", control.RequestID)
-		control.StopControl()
-		// TODO close the user connection? this does not trigger a shutdown of the input
+		control.ControlPub.Close()
 		// TODO better solution than allowing existing streams to stomp over one another
 	}
 
