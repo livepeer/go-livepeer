1dfaa399d6456b401548080d47b1bdd4935ae1a0
hjpotter92 <hjpotter92@users.noreply.github.com>
2025-04-22 13:46:15 +0530

    Fix warnings reported by editorconfig (#3525)

--- cmd/devtool/scripts/create_multiple_transcoders.bash
+++ cmd/devtool/scripts/create_multiple_transcoders.bash
@@ -6,8 +6,8 @@ RTMP_PORT=1935
 BOND=50
 
 if [ -z "$1" ]; then
-    echo "Usage: create_multiple_transcoders.bash <num_transcoders>"
-    exit 1
+  echo "Usage: create_multiple_transcoders.bash <num_transcoders>"
+  exit 1
 fi
 
 num_transcoders=$1
@@ -15,12 +15,12 @@ num_transcoders=$1
 # Run the go devtool transcoder script multiple times with different ports
 echo "Creating $num_transcoders transcoders..."
 for i in $(seq 1 $num_transcoders); do
-    echo "Creating transcoder $i..."
-    go run cmd/devtool/devtool.go --cliport $CLI_PORT --mediaport $MEDIA_PORT --rtmpport $RTMP_PORT -bond $BOND setup transcoder 
-    CLI_PORT=$((CLI_PORT+10))
-    MEDIA_PORT=$((MEDIA_PORT+10))
-    RTMP_PORT=$((RTMP_PORT+10))
-    BOND=$((BOND**10))
+  echo "Creating transcoder $i..."
+  go run cmd/devtool/devtool.go --cliport $CLI_PORT --mediaport $MEDIA_PORT --rtmpport $RTMP_PORT -bond $BOND setup transcoder
+  CLI_PORT=$((CLI_PORT + 10))
+  MEDIA_PORT=$((MEDIA_PORT + 10))
+  RTMP_PORT=$((RTMP_PORT + 10))
+  BOND=$((BOND ** 10))
 done
 
 echo "Done creating $num_transcoders transcoders"
