8cc886d95356b164bd71e67d64b7def6364e9d69
Brad P <brad@ad-astra.video>
2025-05-19 06:14:16 -0500

    fix payment test

--- server/rpc_test.go
+++ server/rpc_test.go
@@ -839,7 +839,18 @@ func TestGetPayment_GivenInvalidProtoData_ReturnsError(t *testing.T) {
 
 	_, err := getPayment(header)
 
-	assert.Contains(t, err.Error(), "protobuf")
+	assert.Contains(t, err.Error(), "could not parse payment")
+}
+
+func TestGetPayment_JsonData_ReturnsPayment(t *testing.T) {
+	protoPayment := defaultPayment(t)
+	data, _ := json.Marshal(protoPayment)
+
+	header := base64.StdEncoding.EncodeToString(data)
+
+	_, err := getPayment(header)
+
+	assert.Nil(t, err)
 }
 
 func TestGetPayment_GivenValidHeader_ReturnsPayment(t *testing.T) {
