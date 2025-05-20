c4134347f7ec3523de3f858fbd497fe03d897840
Rafał Leszko <rafal@livepeer.org>
2025-05-20 09:45:52 +0200

    Fix condition

--- server/ai_process.go
+++ server/ai_process.go
@@ -1532,7 +1532,6 @@ func processAIRequest(ctx context.Context, params aiRequestParams, req interface
 			break
 		}
 		sessTries[sess.Transcoder()]++
-		params.liveParams.orchestrator = "128.0.0.1"
 		if params.liveParams.orchestrator != "" && !strings.Contains(sess.Transcoder(), params.liveParams.orchestrator) {
 			// user requested a specific orchestrator, so ignore all the others
 			clog.Infof(ctx, "Skipping orchestrator=%s because user request specific orchestrator=%s", sess.Transcoder(), params.liveParams.orchestrator)
