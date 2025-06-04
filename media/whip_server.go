cd0e37b93dbb73ff57da1c68a7e88d5d0566cbee
Max Holland <max@livepeer.org>
2025-06-04 17:18:38 +0100

    Revert "Reduce ice timeout (#3610)"
    This reverts commit 0c392e7df93e9178369a8f472ec8200762dd0ece.

--- media/whip_server.go
+++ media/whip_server.go
@@ -33,12 +33,7 @@ import (
 
 // TODO handle PATCH/PUT for ICE restarts (new Offers) and DELETE
 
-const (
-	keyframeInterval       = 2 * time.Second // TODO make configurable?
-	iceKeepAliveInterval   = 2 * time.Second
-	iceFailedTimeout       = 10 * time.Second
-	iceDisconnectedTimeout = 5 * time.Second
-)
+const keyframeInterval = 2 * time.Second // TODO make configurable?
 
 // Generate a random ID for new resources
 func generateID() string {
@@ -85,16 +80,9 @@ func (s *WHIPServer) CreateWHIP(ctx context.Context, ssr *SwitchableSegmentReade
 	}
 	defer r.Body.Close()
 
-	settingEngine := webrtc.SettingEngine{}
-	settingEngine.SetICETimeouts(
-		iceDisconnectedTimeout,
-		iceFailedTimeout,
-		iceKeepAliveInterval,
-	)
-
 	// Create a new PeerConnection
 	interceptors, statsGetter := genInterceptors(s.mediaEngine)
-	api := webrtc.NewAPI(webrtc.WithMediaEngine(s.mediaEngine), interceptors, s.settings, webrtc.WithSettingEngine(settingEngine))
+	api := webrtc.NewAPI(webrtc.WithMediaEngine(s.mediaEngine), interceptors, s.settings)
 	peerConnection, err := api.NewPeerConnection(WebrtcConfig)
 	if err != nil {
 		clog.InfofErr(ctx, "Failed to create peerconnection", err)
