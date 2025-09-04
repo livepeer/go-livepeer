4bd686f718a0a193aded813409b3c7aa49371b65
Victor Elias <victor@livepeer.org>
2025-09-04 18:16:39 +0000

    box: Allow changing input video FPS

--- box/stream.sh
+++ box/stream.sh
@@ -5,6 +5,7 @@ PIPELINE=${PIPELINE:-noop}
 STREAM_KEY="my-stream"
 STREAM_ID="my-stream-id"
 RTMP_OUTPUT=${RTMP_OUTPUT:-""}
+FPS=${FPS:-30}
 
 case "$1" in
   start)
@@ -19,11 +20,11 @@ case "$1" in
     fi
 
     ffmpeg -re -f lavfi \
-      -i testsrc=size=1920x1080:rate=30,format=yuv420p \
+      -i testsrc=size=1280x720:rate=${FPS},format=yuv420p \
       -vf scale=1280:720 \
       -c:v libx264 \
       -b:v 1000k \
-      -x264-params keyint=60 \
+      -x264-params keyint=$((FPS * 2)) \
       -f flv rtmp://127.0.0.1:1935/${STREAM_KEY}?${QUERY}
     ;;
   playback)
