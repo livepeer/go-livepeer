1f0c7e990e08d2b0ae24e02c58dcaef11b5905a4
Victor Elias <victor@livepeer.org>
2025-09-19 21:08:41 +0000

    ai/box: Allow box to run any streamdiffusion flavour

--- box/build-runner.sh
+++ box/build-runner.sh
@@ -3,8 +3,8 @@ set -ex
 
 PIPELINE=${PIPELINE:-noop}
 
-if [[ "$PIPELINE" != "noop" && "$PIPELINE" != "comfyui" && "$PIPELINE" != "streamdiffusion" && "$PIPELINE" != "streamdiffusion-sd15" ]]; then
-  echo "Error: PIPELINE must be either 'noop', 'comfyui', 'streamdiffusion' or 'streamdiffusion-sd15'"
+if [[ "$PIPELINE" != "noop" && "$PIPELINE" != "comfyui" && "$PIPELINE" != "streamdiffusion" && ! "$PIPELINE" =~ ^streamdiffusion- ]]; then
+  echo "Error: PIPELINE must be either 'noop', 'comfyui', 'streamdiffusion' or start with 'streamdiffusion-'"
   exit 1
 fi
 
@@ -20,9 +20,10 @@ if [ "${PIPELINE}" = "noop" ]; then
 else
     BASE_PIPELINE=${PIPELINE}
     INFERPY_INITIAL_PARAMS=""
-    if [[ "$PIPELINE" == "streamdiffusion-sd15" ]]; then
+    if [[ "$PIPELINE" =~ ^streamdiffusion- ]]; then
         BASE_PIPELINE="streamdiffusion"
-        JSON_FILE=./app/live/pipelines/streamdiffusion_sd15_default_params.json
+        FILE_NAME="${PIPELINE//-/_}"
+        JSON_FILE=./app/live/pipelines/${FILE_NAME}_default_params.json
         INFERPY_INITIAL_PARAMS=$(tr -d '\n' < "$JSON_FILE")
     fi
     docker build -t livepeer/ai-runner:live-base-${BASE_PIPELINE} -f docker/Dockerfile.live-base-${BASE_PIPELINE} .
