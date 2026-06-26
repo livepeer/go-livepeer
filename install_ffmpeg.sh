4353a763d027e0de2a49cc5f46e4ea853a5c4243
Josh Allmann <joshua.allmann@gmail.com>
2026-06-26 14:57:56 -0700

    update lpms again

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo 'WARNING: downloading and executing lpms/install_ffmpeg.sh, use it directly in case of issues'
-curl https://raw.githubusercontent.com/livepeer/lpms/dca1710ae43850b2ef752815a060d359ed597f7f/install_ffmpeg.sh | bash -s $1
+curl https://raw.githubusercontent.com/livepeer/lpms/bc67cafe732afb08053328483f73cf000a28d330/install_ffmpeg.sh | bash -s $1
